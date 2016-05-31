e2c1e3da490e39ad88479789790cc45c4b31a445
trungnghia112 <trungnghia112@gmail.com>
2016-05-31 10:27:45 +0700

    [modify] node static-server

--- static-server.js
+++ static-server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var port = 4000;
+var port = 3000;
 
 http.createServer(function (request, response) {
     console.log('request starting...');
