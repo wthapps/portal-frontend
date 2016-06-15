dcd30b5eceedd7fa1c6b5c6617ed633f94b3f2ff
trungnghia112 <trungnghia112@gmail.com>
2016-06-15 15:58:54 +0700

    [modify] account setting

--- src/client/app/account/setting/my-account.component.ts
+++ src/client/app/account/setting/my-account.component.ts
@@ -30,24 +30,29 @@ export class MyAccountComponent {
   pageTitle:string = 'Account setting';
 
   updateInfo:ControlGroup;
+  //dateRange:Array;
 
   constructor(private _userService:UserService,
               private _builder:FormBuilder) {
+    console.log(this._userService.profile);
+    //let birthday_day = new Date(this._userService.profile.birthday);
+
+    //this.dateRange = Array(31).fill().map((x,i)=>i);
+
     this.updateInfo = _builder.group({
       first_name: [this._userService.profile.first_name,
-        Validators.compose([Validators.required, CustomValidators.emailFormat])
+        Validators.required
+      ],
+      last_name: [this._userService.profile.last_name,
+        Validators.required
       ],
       email: [this._userService.profile.email,
         Validators.compose([Validators.required, CustomValidators.emailFormat]),
         CustomValidators.duplicated
-      ],
-      email: [this._userService.profile.email,
-        Validators.compose([Validators.required, CustomValidators.emailFormat]),
-        CustomValidators.duplicated
-      ],
-      password: ['',
-        Validators.compose([Validators.required])
-      ]
+      ]/*,
+      birthday_day: [birthday_day.getDate()],
+      birthday_month: [birthday_day.getMonth()],
+      birthday_year: [birthday_day.getUTCFullYear()]*/
     });
   }
 }
