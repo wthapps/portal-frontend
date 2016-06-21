bd1f1bdb294069de3380d01f7dbb25e49b5639ad
trungnghia112 <trungnghia112@gmail.com>
2016-06-21 09:40:58 +0700

    [modify] account setting

--- src/client/app/account/setting/my-account.component.ts
+++ src/client/app/account/setting/my-account.component.ts
@@ -30,6 +30,15 @@ export class MyAccountComponent {
   pageTitle:string = 'Account setting';
 
   updateInfo:ControlGroup;
+
+  birthdayDate:Object = {
+    day: 0,
+    month: 0,
+    year: 0
+  };
+
+  sex:number;
+
   //dateRange:Array;
 
   constructor(private _userService:UserService,
@@ -41,6 +50,17 @@ export class MyAccountComponent {
     }
 
     console.log(this._userService.profile);
+
+    this.sex = this._userService.profile.sex;
+
+    if (this._userService.profile.birthday !== null) {
+      let birthday = new Date(this._userService.profile.birthday);
+      this.birthdayDate.day = birthday.getDate();
+      this.birthdayDate.month = birthday.getMonth();
+      this.birthdayDate.year = birthday.getUTCFullYear();
+    }
+    console.log(this.birthdayDate.day, this.birthdayDate.month, this.birthdayDate.year);
+
     this.updateInfo = this._builder.group({
       first_name: [this._userService.profile.first_name,
         Validators.required
@@ -51,10 +71,11 @@ export class MyAccountComponent {
       email: [this._userService.profile.email,
         Validators.compose([Validators.required, CustomValidators.emailFormat]),
         CustomValidators.duplicated
-      ]/*,
-       birthday_day: [birthday_day.getDate()],
-       birthday_month: [birthday_day.getMonth()],
-       birthday_year: [birthday_day.getUTCFullYear()]*/
+      ],
+      birthday_day: [this.birthdayDate.day],
+      birthday_month: [this.birthdayDate.month],
+      birthday_year: [this.birthdayDate.year],
+      sex: [],
     });
 
   }
