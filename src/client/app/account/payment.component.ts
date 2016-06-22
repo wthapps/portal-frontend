2b7336652fdb78434a450deaf176a6bb1502dd16
trungnghia112 <trungnghia112@gmail.com>
2016-06-22 17:33:21 +0700

    [modify] style braintree payment page

--- src/client/app/account/payment.component.ts
+++ src/client/app/account/payment.component.ts
@@ -121,10 +121,23 @@ export class PaymentComponent implements AfterViewInit {
         client: clientInstance,
         styles: {
           'input': {
-            'color': '#282c37',
-            'font-size': '16px',
-            'transition': 'color 0.1s',
-            'line-height': '3'
+            'display': 'block',
+            'width': '100%',
+            'height': '34px',
+            'padding': '6px 12px',
+            'font-size': '14px',
+            'line-height': '1.428571429',
+            'color': '#555555',
+            'background-color': '#fff',
+            'background-image': 'none',
+            'border': '1px solid #ccc',
+            'border-radius': '4px',
+            '-webkit-box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
+            '-moz-box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
+            'box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
+            '-webkit-transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s',
+            '-moz-transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s',
+            'transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s'
           },
           // Style the text of an invalid input
           'input.invalid': {
@@ -132,16 +145,16 @@ export class PaymentComponent implements AfterViewInit {
           },
           // placeholder styles need to be individually adjusted
           '::-webkit-input-placeholder': {
-            'color': 'rgba(0,0,0,0.6)'
+            'color': '#999'
           },
           ':-moz-placeholder': {
-            'color': 'rgba(0,0,0,0.6)'
+            'color': '#999'
           },
           '::-moz-placeholder': {
-            'color': 'rgba(0,0,0,0.6)'
+            'color': '#999'
           },
           ':-ms-input-placeholder ': {
-            'color': 'rgba(0,0,0,0.6)'
+            'color': '#999'
           }
 
         },
