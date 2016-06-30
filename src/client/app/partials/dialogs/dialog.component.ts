7c5bd5d364b030d3a74f464f103f6679f4ff61d9
trungnghia112 <trungnghia112@gmail.com>
2016-06-30 14:02:07 +0700

    [modify] dialog center

--- src/client/app/partials/dialogs/dialog.component.ts
+++ src/client/app/partials/dialogs/dialog.component.ts
@@ -11,6 +11,7 @@ const KEY_ESC = 27;
   styleUrls: ['dialog.component.css']
 })
 export class DialogComponent implements OnInit {
+  longModal:boolean;
   title:string;
   message:string;
   okText:string;
@@ -19,11 +20,13 @@ export class DialogComponent implements OnInit {
   positiveOnClick:(e:any) => void;
 
   private defaults = {
+    longModal: false,
     title: 'Confirmation',
     message: 'Are you sure?',
     cancelText: 'Cancel',
     okText: 'OK'
   };
+  private bodyElement:any;
   private modalElement:any;
   private modalElementBackdrop:any;
   private cancelButton:any;
@@ -36,11 +39,13 @@ export class DialogComponent implements OnInit {
   activate(message = this.defaults.message,
            title = this.defaults.title,
            okText = this.defaults.okText,
-           cancelText = this.defaults.cancelText) {
+           cancelText = this.defaults.cancelText,
+           longModal = this.defaults.longModal) {
     this.title = title;
     this.message = message;
     this.okText = okText;
     this.cancelText = cancelText;
+    this.longModal = longModal;
 
     let promise = new Promise<boolean>((resolve, reject) => {
       this.negativeOnClick = (e:any) => resolve(false);
@@ -52,6 +57,7 @@ export class DialogComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.bodyElement = document.body;
     this.modalElement = document.getElementById('confirmationModal');
     this.modalElementBackdrop = document.getElementById('confirmationModal-backdrop');
     this.cancelButton = document.getElementById('cancelButton');
@@ -96,17 +102,43 @@ export class DialogComponent implements OnInit {
     };
 
     //this.modalElement.style.opacity = 1;
+    this.bodyElement.classList.add('modal-open');
     this.modalElement.classList.add('in');
     this.modalElement.style.display = 'block';
+    this.bodyElement.style.paddingRight = this.getScrollbarWidth()+'px';
   }
 
   private hideDialog() {
     document.onkeyup = null;
     //this.modalElement.style.opacity = 0;
     //window.setTimeout(() => this.modalElement.style.zIndex = 0, 400);
-
+    this.bodyElement.classList.remove('modal-open');
     this.modalElement.style.display = 'none';
     this.modalElement.classList.remove('in');
     this.modalElementBackdrop.classList.remove('in');
+    this.bodyElement.style.paddingRight = 0;
+  }
+
+  private getScrollbarWidth() {
+    var outer = document.createElement("div");
+    outer.style.visibility = "hidden";
+    outer.style.width = "100px";
+    document.body.appendChild(outer);
+
+    var widthNoScroll = outer.offsetWidth;
+    // force scrollbars
+    outer.style.overflow = "scroll";
+
+    // add innerdiv
+    var inner = document.createElement("div");
+    inner.style.width = "100%";
+    outer.appendChild(inner);
+
+    var widthWithScroll = inner.offsetWidth;
+
+    // remove divs
+    outer.parentNode.removeChild(outer);
+
+    return widthNoScroll - widthWithScroll;
   }
 }
--- src/client/app/partials/dialogs/dialog.service.ts
+++ src/client/app/partials/dialogs/dialog.service.ts
@@ -2,5 +2,16 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class DialogService {
-  activate:(message?:string, title?:string, okText?:string, cancelText?:string) => Promise<boolean>;
+  /**
+   * Information about a dialog.
+   *
+   * It has the following properties:
+   * - `message` is a content of dialog
+   * - `title` is a title of dialog
+   * - `okText` is a text of ok
+   * - `cancelText` is a title of cancel
+   * - `longModal` is a type of dialog, if message is too many line.
+   *
+   */
+  activate:(message?:string, title?:string, okText?:string, cancelText?:string, longModal?:boolean) => Promise<boolean>;
 }
