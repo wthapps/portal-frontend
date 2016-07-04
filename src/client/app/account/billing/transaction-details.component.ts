3a8dcfc42d578df84154c31e21a09147f88bc0cd
trungnghia112 <trungnghia112@gmail.com>
2016-07-04 14:41:59 +0700

    [modify] transaction detail

--- src/client/app/account/billing/transaction-details.component.ts
+++ src/client/app/account/billing/transaction-details.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit, Pipe, PipeTransform}   from '@angular/core';
+import {Component, OnInit, Pipe, PipeTransform,
+  ElementRef
+}                                                 from '@angular/core';
 import {ROUTER_DIRECTIVES, Router, RouteSegment}  from '@angular/router';
 import {UserService, CONFIG}                      from '../../shared/index';
 import {TransactionService}                       from '../transaction.service';
@@ -19,7 +21,34 @@ import {LoadingService}                           from '../../partials/loading/l
 
 export class TransactionDetailsComponent implements OnInit{
   pageTitle:string = 'Transaction details';
-  trasaction: any;
+  transaction: any = {
+    amount: 0,
+    merchant: '',
+    currency_iso_code: '',
+    created_at: new Date(),
+    updated_at: '',
+    status: '',
+    type: '',
+    payment_instrument_type: '',
+    billing_address: {
+      street_address: '',
+      extended_address: '',
+      region: '',
+      postal_code: '',
+      locality: '',
+      country_name: ''
+    },
+    customer: {
+      firt_name: '',
+      last_name: '',
+      email: ''
+    },
+    credit_card: {
+      last_4: '',
+      card_type: '',
+      cardholder_name: ''
+    }
+  };
   trans_id: string;
 
   constructor(
@@ -27,16 +56,29 @@ export class TransactionDetailsComponent implements OnInit{
     private _router:Router,
     private _routeSegment: RouteSegment,
     private _transactionService: TransactionService,
-    private _loadingService: LoadingService
+    private _loadingService: LoadingService,
+    private _el:ElementRef
   ) {
     if (!this._userService.loggedIn) {
       this._router.navigateByUrl(`/login;${CONFIG.params.next}=${this._router._location.path().replace(/\//g, '\%20')}`);
     }
     this.trans_id = this._routeSegment.getParam('id');
+    console.log(this.trans_id);
   }
 
   ngOnInit(): void {
-
+    this.trans_id = this._routeSegment.getParam('id');
+    this._loadingService.start();
+    this._transactionService.list(`users/${this._userService.profile.id}/transactions/${this.trans_id}`)
+      .subscribe((response) => {
+          this.transaction = response.data;
+          this.transaction.created_at = new Date(response.data.created_at);
+          this._loadingService.stop();
+        },
+        error => {
+          console.log("Receipt error", error);
+          this._loadingService.stop();
+        });
   }
 
   viewReceipt(event: any): void {
