39202abf278c0384beb25649c6974a79bf76683f
trungnghia112 <trungnghia112@gmail.com>
2016-07-05 10:41:02 +0700

    [modify] toast message

---
+++ src/client/app/partials/toast/top-message.service.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ToastsService {
+  info:(message?:string, close?:boolean) => Promise<boolean>;
+  danger:(message?:string, close?:boolean) => Promise<boolean>;
+  warning:(message?:string, close?:boolean) => Promise<boolean>;
+  success:(message?:string, close?:boolean) => Promise<boolean>;
+}
