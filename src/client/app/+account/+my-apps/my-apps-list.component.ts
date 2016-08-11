3a945e0ad9422cff6d9b1b6737b782d8fd7e42cf
trungnghia112 <trungnghia112@gmail.com>
2016-08-11 17:32:07 +0700

    [add] my apps page
    refs #WTHAP-7

--- src/client/app/+account/+my-apps/my-apps-list.component.ts
+++ src/client/app/+account/+my-apps/my-apps-list.component.ts
@@ -4,33 +4,37 @@ import {
 }                       from '@angular/core';
 
 import { ApiBaseService, UserService } from '../../shared/index';
- 
+
 
 @Component({
   moduleId: module.id,
   templateUrl: 'my-apps-list.component.html',
-  directives: [    
+  directives: [
   ]
 })
 
 export class MyAppsListComponent implements OnInit {
-  panelTitle: string = 'Find Services or Add-ons';
+  pageTitle: string = 'App List';
   my_apps: any;
 
-  constructor( 
-    private apiService: ApiBaseService, private userService: UserService 
+  errorMessage: string;
+
+  my_apps: Array<any> = [];
+
+  constructor(
+    private apiService: ApiBaseService, private userService: UserService
   ) {
   }
 
-  ngOnInit():void {    
+  ngOnInit():void {
     this.apiService.get(`users/${this.userService.profile.id}/my-apps`).subscribe(
-      response => {
-        this.my_apps = response.data;        
+      (response:any) => {
+        this.my_apps = response.data;
       },
       error => {
         this.errorMessage = <any>error
       }
     );
-  }  
-  
+  }
+
 }
