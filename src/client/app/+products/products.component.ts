5e41858d69bbb28b6166a0002a9be63151706c08
trungnghia112 <trungnghia112@gmail.com>
2016-09-01 15:27:15 +0700

    [add] products page
    refs #WTHAP-2

--- src/client/app/+products/products.component.ts
+++ src/client/app/+products/products.component.ts
@@ -1,15 +1,20 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
+import {
+  FooterPromotionComponent
+} from '../shared/index';
+
 @Component({
   moduleId: module.id,
   templateUrl: 'products.component.html',
   directives: [
-    ROUTER_DIRECTIVES
+    ROUTER_DIRECTIVES,
+    FooterPromotionComponent
   ]
 })
 
 export class ProductsComponent{
   pageTitle:string = 'Products';
-  
+
 }
