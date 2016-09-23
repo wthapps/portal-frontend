84348fa0a47c40b6d09918f01444ca308f3353fc
trungnghia112 <trungnghia112@gmail.com>
2016-09-23 17:23:19 +0700

    [modify] zone picture photo detail -connect data
    refs #WTHZONE-4

--- src/client/app/+zone/+picture/+photo/photo.component.ts
+++ src/client/app/+zone/+picture/+photo/photo.component.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit, OnChanges} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
-import {ZPhotoDetailComponent} from './photo-detail.component'
+import {ZPhotoDetailComponent} from './photo-detail.component';
+import {Photo} from '../../../shared/models/photo.model';
+import {ApiBaseService, UserService} from '../../../shared/index';
 
 
 @Component({
@@ -14,31 +16,37 @@ import {ZPhotoDetailComponent} from './photo-detail.component'
   ]
 })
 
-export class ZPhotoComponent {
+export class ZPhotoComponent implements OnInit, OnChanges {
+  @Input() listFilter: any;
+
+  errorMessage: string = '';
+
   showImg: boolean = false;
 
-  imgSrcDetail: string = '/assets/images/zone/img-default.png';
+  imgId: number;
 
-  dataImages: Array<any> = [
-    {
-      id: 1,
-      img_thumb: 'http://www.catprotection.com.au/wp-content/uploads/2014/11/5507692-cat-m.jpg',
-      img_large: 'http://www.catprotection.com.au/wp-content/uploads/2014/11/5507692-cat-m.jpg'
-    },
-    {
-      id: 2,
-      img_thumb: 'https://s-media-cache-ak0.pinimg.com/736x/a9/a3/46/a9a34606f68f5f86aa94833ad482e4c9.jpg',
-      img_large: 'https://s-media-cache-ak0.pinimg.com/736x/a9/a3/46/a9a34606f68f5f86aa94833ad482e4c9.jpg'
-    },
-    {
-      id: 3,
-      img_thumb: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/06/pallas-cat-manul-14__880.jpg',
-      img_large: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/06/pallas-cat-manul-14__880.jpg'
-    }
-  ];
+  dataImages: Array<Photo> = [];
 
-  onClick(src): void {
-    this.imgSrcDetail = src;
+  constructor(private apiService: ApiBaseService, private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.apiService.get(`${this.userService.profile.id}/zone/photos`).subscribe(
+      (response: any) => {
+        this.dataImages = response.data;
+      },
+      error => {
+        this.errorMessage = <any>error;
+      }
+    );
+  }
+
+  ngOnChanges() {
+    console.log(this.listFilter);
+  }
+
+  onClick(id): void {
+    this.imgId = id;
     this.showImg = true;
   }
 
