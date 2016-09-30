29546b342299d7c0a635cfa71fba8395a3f31748
trungnghia112 <trungnghia112@gmail.com>
2016-09-30 16:44:36 +0700

    [add] Select photos to create album
    refs #WTHZONE-48

--- src/client/app/+zone/+picture/+photo/photo.component.ts
+++ src/client/app/+zone/+picture/+photo/photo.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ElementRef, OnChanges, SimpleChange} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
 import {ZPictureGridComponent} from '../shared/grid.component';
@@ -29,7 +29,7 @@ declare var _: any;
   ]
 })
 
-export class ZPhotoComponent implements OnInit, OnChanges {
+export class ZPhotoComponent implements OnInit {
   errorMessage: string = '';
 
   showImg: boolean = false;
@@ -51,9 +51,6 @@ export class ZPhotoComponent implements OnInit, OnChanges {
     this.getPhotos(this.currentPage);
   }
 
-  ngOnChanges(changes: SimpleChange){
-      console.log("changing..........");
-  }
   ngAfterViewInit() {
     let win = $(window);
     let _this = this;
@@ -68,11 +65,12 @@ export class ZPhotoComponent implements OnInit, OnChanges {
     });
   }
 
-  getPhotos(page:any) {
+  getPhotos(page: any) {
     if (this.currentPage <= Math.ceil(this.total / this.perPage)) {
       this.loadingService.start('#photodata-loading');
       this.apiService.get(`${this.userService.profile.id}/zone/photos?page=${page}`).subscribe(
         (response: any) => {
+          console.log(response);
           this.perPage = response.per_page;
           this.total = response.total;
           this.dataImages = _.concat(this.dataImages, response.data);
@@ -86,7 +84,7 @@ export class ZPhotoComponent implements OnInit, OnChanges {
     }
   }
 
-  onClick(id:any): void {
+  onClick(id: any): void {
     console.log(id);
     this.imgId = id;
     this.showImg = true;
--- src/client/app/+zone/+picture/shared/grid.component.ts
+++ src/client/app/+zone/+picture/shared/grid.component.ts
@@ -1,7 +1,15 @@
-import {Component, Input, Output, EventEmitter, OnChanges} from '@angular/core';
+import {
+  Component, Input, Output,
+  EventEmitter, OnChanges,
+  //HostListener,
+  KeyboardEvent
+}                          from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
 import {Photo} from '../../../shared/models/photo.model';
+
+declare var $: any;
+declare var _: any;
 
 @Component({
   moduleId: module.id,
@@ -9,19 +17,71 @@ import {Photo} from '../../../shared/models/photo.model';
   templateUrl: 'grid.component.html',
   directives: [
     ROUTER_DIRECTIVES
-  ]
+  ],
+  host: {
+    '(document:keydown)': 'onDocumentKeyDown($event)',
+    '(document:keyup)': 'onDocumentKeyUp($event)',
+  }
 })
 
 export class ZPictureGridComponent implements OnChanges {
   @Input() data: Array<Photo>;
   @Output() imgDetail: EventEmitter<number> = new EventEmitter<number>();
   dataImages: Array<Photo> = [];
+  dataAddImages: Array<any> = [];
+
+  keyCtrl: boolean = false;
+
+
+  /*@HostListener('document:keyup', ['$event']);
+   onKeyUp(ev:KeyboardEvent) {
+   // do something meaningful with it
+   console.log(`The user just pressed ${ev.key}!`);
+   }*/
+
+  onDocumentKeyDown(ev: KeyboardEvent) {
+    if (ev.key == 'Control' || ev.key == 'Meta') {
+      this.keyCtrl = true;
+    }
+  }
+
+  onDocumentKeyUp(ev: KeyboardEvent) {
+    if (ev.key == 'Control' || ev.key == 'Meta') {
+      this.keyCtrl = false;
+    }
+  }
 
   ngOnChanges() {
     this.dataImages = this.data;
   }
 
-  onClick(id:any) {
+  ondbClick(id: any) {
     this.imgDetail.emit(id);
+  }
+
+  /**
+   *
+   * @param e
+   * @param id of Image
+     */
+  onSelected(e: any, id: number) {
+    let parent = $(e.target).parents('.row-img');
+    let el = $(e.target).parents('.photo-box-img');
+
+    // checking multi selection
+    if (!this.keyCtrl) {
+      this.dataAddImages = [];
+      parent.find('.photo-box-img').removeClass('selected');
+    }
+
+    el.toggleClass('selected');
+
+    // checking item exists in array
+    if (_.indexOf(this.dataAddImages, id) > 0) {
+      _.pull(this.dataAddImages, id); // remove
+    } else {
+      this.dataAddImages.push(id); // add
+    }
+    console.log(this.dataAddImages);
   }
 }
