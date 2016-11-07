2761ec999b6eeb9d7cd74b25513f94ab638aedd1
trungnghia112 <trungnghia112@gmail.com>
2016-11-07 18:09:33 +0700

    [modify] zone social photo detail

--- src/client/app/zone/social/post-item/post-item-layout/post-item-footer.component.ts
+++ src/client/app/zone/social/post-item/post-item-layout/post-item-footer.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild, OnInit, Input } from '@angular/core';
-import {BaseZoneSocialItem} from "../../base/base-social-item";
-import {SoPost} from "../../../../shared/models/social_network/so-post.model";
+import { Component, ViewChild, OnInit, Input, OnChanges } from '@angular/core';
+import { BaseZoneSocialItem } from "../../base/base-social-item";
+import { SoPost } from "../../../../shared/models/social_network/so-post.model";
 
 declare var _: any;
 
@@ -14,17 +14,25 @@ declare var _: any;
   }
 })
 
-export class ZSocialPostItemFooterComponent extends BaseZoneSocialItem{
+export class ZSocialPostItemFooterComponent extends BaseZoneSocialItem implements OnChanges{
   @Input() item: SoPost;
   @Input() type: string;
+
+  showInfo: boolean = false;
+
+  ngOnChanges() {
+    if (this.type == 'info') {
+      this.showInfo = true;
+    }
+  }
 
   // onDocumentKeyDown(ev: KeyboardEvent) {
   //   console.log(ev);
   //   // if (ev.keyCode == 17 || ev.keyCode == 18 || ev.keyCode == 91 || ev.keyCode == 93 || ev.ctrlKey) {
   //   //   this.keyCtrl = true;
   // }
-  onKey(e:KeyboardEvent) {
-    if(e.keyCode == 13 && e.target.value != "") {
+  onKey(e: KeyboardEvent) {
+    if (e.keyCode == 13 && e.target.value != "") {
       console.log('create ' + e.target.value);
     }
   }
