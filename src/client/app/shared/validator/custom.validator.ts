e0ee473f5d394aba1789f6516909dec4b4c998da
trungnghia112 <trungnghia112@gmail.com>
2016-12-06 11:33:37 +0700

    [modify] zone social communities edit additional links

--- src/client/app/shared/validator/custom.validator.ts
+++ src/client/app/shared/validator/custom.validator.ts
@@ -16,17 +16,19 @@ export class CustomValidator {
     return EMAIL_REGEXP.test(c.value) ? null : {'emailFormat': true};
   }
 
+  public static urlFormat(c: AbstractControl) {
+    let REGEXP = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+    return REGEXP.test(c.value) ? null : {'url': true};
+  }
+
   public static url(control: AbstractControl) {
     let obj: any = Validators.required(control);
     if (obj !== undefined && obj !== null) return null;
 
     let v: string = control.value;
     return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(v) ? null : {'url': true};
-
-
-    // let REGEXP = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
-    // return REGEXP.test(c.value) ? null : {'url': true};
   }
+
   // refs Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet and 1 Number:
   public static passwordCheck(c: AbstractControl) {
     let PASS_REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i;
