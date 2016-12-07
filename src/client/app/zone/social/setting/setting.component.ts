4c58c31da04f1ab9ee0a11e31f9ae22e031d92f3
trungnghia112 <trungnghia112@gmail.com>
2016-12-07 14:02:11 +0700

    [modify] zone social add confirm message when user reset default settings

--- src/client/app/zone/social/setting/setting.component.ts
+++ src/client/app/zone/social/setting/setting.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { SocialService } from '../services/social.service';
 import { SoUser } from '../../../shared/models/social_network/so-user.model';
 
+import {
+  ConfirmationService
+}                           from '../../../shared/index';
+
 @Component({
   moduleId: module.id,
   selector: 'z-social-setting',
@@ -11,26 +15,36 @@ export class ZSocialSettingComponent implements OnInit {
 
   user: SoUser = new SoUser();
 
-  constructor(private socialService: SocialService) {
+  constructor(private socialService: SocialService,
+              private confirmationService: ConfirmationService) {
   }
 
   ngOnInit() {
     this.socialService.user.get().subscribe((res: any) => {
-      this.user = new SoUser().from(res.data);
+        this.user = new SoUser().from(res.data);
       },
     );
   }
 
-  updateSettings(settings:any) {
+  updateSettings(settings: any) {
     this.socialService.user.update({settings: settings}).subscribe((res: any) => {
-      this.user = new SoUser().from(res.data);
-      },
-    );
-  }
-  resetSettings() {
-    this.socialService.user.reset_setting().subscribe((res: any) => {
         this.user = new SoUser().from(res.data);
       },
     );
+  }
+
+  resetSettings() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to reset settings',
+      header: 'Reset Default',
+      accept: () => {
+        this.socialService.user.reset_setting().subscribe((res: any) => {
+            this.user = new SoUser().from(res.data);
+          },
+        );
+      }
+    });
+
+
   }
 }
