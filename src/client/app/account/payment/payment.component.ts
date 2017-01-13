c3958181c43fd75d1d60fe79b7cd85ff614c6a0d
Thinh Huynh <hdthinh@gmail.com>
2017-01-13 22:04:09 +0700

    update create paypal button

--- src/client/app/account/payment/payment.component.ts
+++ src/client/app/account/payment/payment.component.ts
@@ -41,8 +41,6 @@ export class PaymentComponent implements AfterViewInit, OnInit {
   credit_card: CreditCard = null;
   submitted: boolean = false;
 
-  methodPaypal: boolean = false;
-
   paymentForm: FormGroup;
   payment_method: AbstractControl = null;
   cardholder_name: AbstractControl = null;
@@ -54,9 +52,9 @@ export class PaymentComponent implements AfterViewInit, OnInit {
   postcode: AbstractControl = null;
 
   selected_plan: any = null;
-
   private next: string = '';
   private operation: string = '';
+  private paymentMethod: string = 'credit';
 
 
   constructor(private router: Router,
@@ -135,10 +133,11 @@ export class PaymentComponent implements AfterViewInit, OnInit {
   }
 
   ngAfterViewInit() {
-
+    let clientToken: string = '';
     var _this = this;
     var form: any = document.querySelector('#payment-method-card');
     var submit = document.querySelector('#button-pay');
+    var paypalButton = document.querySelector('#paypal-button');
 
 
     // billing address information
@@ -150,150 +149,187 @@ export class PaymentComponent implements AfterViewInit, OnInit {
     var postcode = $('#postcode');
     var country = $('#country');
 
-    braintree.client.create({
-      //authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b'
-      authorization: 'sandbox_s2jx657k_szyg6zhyvyn2grsq'
-    }, function (err: any, clientInstance: any) {
-      if (err) {
-        console.error(err);
-        return;
-      }
 
-      // Create input fields and add text styles
-      braintree.hostedFields.create({
-        client: clientInstance,
-        styles: {
-          'input': {
-            'display': 'block',
-            'width': '100%',
-            'height': '34px',
-            'padding': '6px 12px',
-            'font-size': '14px',
-            'line-height': '1.428571429',
-            'color': '#555555',
-            'background-color': '#fff',
-            'background-image': 'none',
-            'border': '1px solid #ccc',
-            'border-radius': '4px',
-            '-webkit-box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
-            '-moz-box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
-            'box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
-            '-webkit-transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s',
-            '-moz-transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s',
-            'transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s'
-          },
-          // Style the text of an invalid input
-          'input.invalid': {
-            'color': '#E53A40'
-          },
-          // placeholder styles need to be individually adjusted
-          '::-webkit-input-placeholder': {
-            'color': '#999'
-          },
-          ':-moz-placeholder': {
-            'color': '#999'
-          },
-          '::-moz-placeholder': {
-            'color': '#999'
-          },
-          ':-ms-input-placeholder ': {
-            'color': '#999'
+    // getting payment token
+    this.apiBaseService.get(`users/payments/client_token?type=${this.paymentMethod}`).subscribe(
+      (response: any) => {
+        clientToken = response.data;
+
+        braintree.client.create({
+          //authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b'
+          authorization: clientToken
+        }, function (err: any, clientInstance: any) {
+          if (err) {
+            console.error(err);
+            return;
           }
 
-        },
-        // Add information for individual fields
-        fields: {
-          number: {
-            selector: '#card-number',
-            placeholder: '4111 1111 1111 1111'
-          },
-          cvv: {
-            selector: '#cvv',
-            placeholder: '123'
-          },
-          expirationDate: {
-            selector: '#expiration-date',
-            placeholder: '10 / 2019'
-          }
-        }
-      }, function (err: any, hostedFieldsInstance: any) {
-        if (err) {
-          console.error(err);
-          return;
-        }
+          // Create input fields and add text styles
+          braintree.hostedFields.create({
+            client: clientInstance,
+            styles: {
+              'input': {
+                'display': 'block',
+                'width': '100%',
+                'height': '34px',
+                'padding': '6px 12px',
+                'font-size': '14px',
+                'line-height': '1.428571429',
+                'color': '#555555',
+                'background-color': '#fff',
+                'background-image': 'none',
+                'border': '1px solid #ccc',
+                'border-radius': '4px',
+                '-webkit-box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
+                '-moz-box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
+                'box-shadow': 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
+                '-webkit-transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s',
+                '-moz-transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s',
+                'transition': 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s'
+              },
+              // Style the text of an invalid input
+              'input.invalid': {
+                'color': '#E53A40'
+              },
+              // placeholder styles need to be individually adjusted
+              '::-webkit-input-placeholder': {
+                'color': '#999'
+              },
+              ':-moz-placeholder': {
+                'color': '#999'
+              },
+              '::-moz-placeholder': {
+                'color': '#999'
+              },
+              ':-ms-input-placeholder ': {
+                'color': '#999'
+              }
 
-        hostedFieldsInstance.on('validityChange', function (event: any) {
-          // Check if all fields are valid, then show submit button
-          var formValid = Object.keys(event.fields).every(function (key: any) {
-            return event.fields[key].isValid;
-          });
-
-
-          if (formValid && _this.paymentForm.valid) {
-            //$('#button-pay').addClass('show-button');
-            $('#button-pay').prop('disabled', false);
-          } else {
-            //$('#button-pay').removeClass('show-button');
-            $('#button-pay').prop('disabled', true);
-          }
-        });
-
-        hostedFieldsInstance.on('empty', function (event: any) {
-          //$('header').removeClass('header-slide');
-          $('#card-image').removeClass();
-          $(form).removeClass();
-        });
-
-        hostedFieldsInstance.on('cardTypeChange', function (event: any) {
-          // Change card bg depending on card type
-          if (event.cards.length === 1) {
-            $(form).removeClass().addClass(event.cards[0].type);
-            $('#card-image').removeClass().addClass(event.cards[0].type);
-            //$('header').addClass('header-slide');
-
-            // Change the CVV length for AmericanExpress cards
-            if (event.cards[0].code.size === 4) {
-              hostedFieldsInstance.setPlaceholder('cvv', '1234');
+            },
+            // Add information for individual fields
+            fields: {
+              number: {
+                selector: '#card-number',
+                placeholder: '4111 1111 1111 1111'
+              },
+              cvv: {
+                selector: '#cvv',
+                placeholder: '123'
+              },
+              expirationDate: {
+                selector: '#expiration-date',
+                placeholder: '10 / 2019'
+              }
             }
-          } else {
-            hostedFieldsInstance.setPlaceholder('cvv', '123');
-          }
-        });
-
-        submit.addEventListener('click', function (event: any) {
-          event.preventDefault();
-
-          hostedFieldsInstance.tokenize(function (err: any, payload: any) {
+          }, function (err: any, hostedFieldsInstance: any) {
             if (err) {
-              _this.toastsService.danger(err.message);
-              console.log('payment', err);
+              console.error(err);
               return;
             }
 
-            // This is where you would submit payload.nonce to your server
-            let body = JSON.stringify({
-              cardholder_name: cardholder_name.val(),
-              nonce: payload.nonce,
-              address_line_1: address_line_1.val(),
-              address_line_2: address_line_2.val(),
-              city: city.val(),
-              postcode: postcode.val(),
-              zipcode: '',
-              region: region.val(),
-              country: country.val()
+            hostedFieldsInstance.on('validityChange', function (event: any) {
+              // Check if all fields are valid, then show submit button
+              var formValid = Object.keys(event.fields).every(function (key: any) {
+                return event.fields[key].isValid;
+              });
+
+
+              if (formValid && _this.paymentForm.valid) {
+                //$('#button-pay').addClass('show-button');
+                $('#button-pay').prop('disabled', false);
+              } else {
+                //$('#button-pay').removeClass('show-button');
+                $('#button-pay').prop('disabled', true);
+              }
             });
 
-            if (_this.edit_mode) {
-              _this.update(_this, body);
-            } else {
-              _this.create(_this, body);
-            }
+            hostedFieldsInstance.on('empty', function (event: any) {
+              //$('header').removeClass('header-slide');
+              $('#card-image').removeClass();
+              $(form).removeClass();
+            });
 
+            hostedFieldsInstance.on('cardTypeChange', function (event: any) {
+              // Change card bg depending on card type
+              if (event.cards.length === 1) {
+                $(form).removeClass().addClass(event.cards[0].type);
+                $('#card-image').removeClass().addClass(event.cards[0].type);
+                //$('header').addClass('header-slide');
+
+                // Change the CVV length for AmericanExpress cards
+                if (event.cards[0].code.size === 4) {
+                  hostedFieldsInstance.setPlaceholder('cvv', '1234');
+                }
+              } else {
+                hostedFieldsInstance.setPlaceholder('cvv', '123');
+              }
+            });
+
+            submit.addEventListener('click', function (event: any) {
+              event.preventDefault();
+
+              hostedFieldsInstance.tokenize(function (err: any, payload: any) {
+                if (err) {
+                  _this.toastsService.danger(err.message);
+                  console.log('payment', err);
+                  return;
+                }
+
+                // This is where you would submit payload.nonce to your server
+                let body = JSON.stringify({
+                  cardholder_name: cardholder_name.val(),
+                  nonce: payload.nonce,
+                  address_line_1: address_line_1.val(),
+                  address_line_2: address_line_2.val(),
+                  city: city.val(),
+                  postcode: postcode.val(),
+                  zipcode: '',
+                  region: region.val(),
+                  country: country.val()
+                });
+
+                if (_this.edit_mode) {
+                  _this.update(_this, body);
+                } else {
+                  _this.create(_this, body);
+                }
+
+              });
+            }, false);
           });
-        }, false);
-      });
-    });
+
+          braintree.paypal.create({
+            client: clientInstance
+          }, function (paypalErr: any, paypalInstance: any) {
+            paypalButton.addEventListener('click', function (event: any) {
+              // Tokenize here!
+              paypalInstance.tokenize({
+                flow: 'vault', // This enables the Vault flow
+                billingAgreementDescription: 'Where is my money',
+                locale: 'en_CA',
+                enableShippingAddress: false,
+                shippingAddressEditable: false,
+              }, function (tokenizeErr: any, payload: any) {
+                console.log('paypal payment');
+                // Send tokenizationPayload.nonce to server
+                // _this.apiBaseService.post('payment/paypal/checkout', {nonce: payload.nonce}).subscribe(
+                //   (res:any) => {
+                //     _this.toastsService.success('success');
+                //   }
+                // )
+              });
+            });
+          });
+        });
+      }
+    );
   }
+
+
+  changePaymentMethod(element: any){
+    this.paymentMethod = element.value;
+  }
+
 
   /*onCancel() {
    var next = this.next === undefined ? '/account' : this.next.replace(/\%20/g, '\/');
@@ -359,47 +395,6 @@ export class PaymentComponent implements AfterViewInit, OnInit {
           console.log('Add card error:', error);
         });
     _this.loaddingService.start();
-  }
-
-  paypal() {
-    // Get token from server
-    this.apiBaseService.get('payment/paypal').subscribe(
-      (res:any) => {
-        this.callBrainTree(res.data);
-      }
-    )
-  }
-
-  callBrainTree(token:any) {
-    var paypalButton = document.querySelector('#paypal-button');
-    // Create a client.
-    let _this = this
-    braintree.client.create({
-      authorization: token
-    }, function (clientErr, clientInstance) {
-      // Create PayPal component
-      braintree.paypal.create({
-        client: clientInstance
-      }, function (paypalErr, paypalInstance) {
-        paypalButton.addEventListener('click', function () {
-          // Tokenize here!
-          paypalInstance.tokenize({
-            flow: 'vault', // This enables the Vault flow
-            billingAgreementDescription: 'Where is my money',
-            locale: 'en_CA',
-            enableShippingAddress: false,
-            shippingAddressEditable: false,
-          }, function (err, tokenizationPayload) {
-            // Send tokenizationPayload.nonce to server
-            _this.apiBaseService.post('payment/paypal/checkout', {nonce: tokenizationPayload.nonce}).subscribe(
-              (res:any) => {
-                _this.toastsService.success('success');
-              }
-            )
-          });
-        });
-      });
-    });
   }
 }
 
