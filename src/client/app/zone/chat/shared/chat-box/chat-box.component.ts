36d92532063dada007c0441329b309c031cddf56
trungnghia112 <trungnghia112@gmail.com>
2017-01-17 16:39:14 +0700

    [modify] zone chat emoji to chatbox

--- src/client/app/zone/chat/shared/chat-box/chat-box.component.ts
+++ src/client/app/zone/chat/shared/chat-box/chat-box.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ZChatEmojiService } from '../emoji/emoji.service';
 
 @Component({
   moduleId: module.id,
   selector: 'z-chat-share-chatbox',
-  templateUrl: 'chat-box.component.html'
+  templateUrl: 'chat-box.component.html',
+  styles: [`
+    .wth-emoji {cursor:pointer;}
+  `]
 })
 
-export class ZChatChatboxComponent {
+export class ZChatChatboxComponent implements OnInit {
 
+  emojiData: any = [];
+
+  ngOnInit() {
+    this.emojiData = ZChatEmojiService.emojis;
+  }
+
+  onEmojiClick(e) {
+    console.log(e);
+  }
 }
