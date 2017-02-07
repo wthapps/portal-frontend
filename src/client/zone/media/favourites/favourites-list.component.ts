f0f55cc14c75f4c09d3fd9d533c395bf9ca16b56
trungnghia112 <trungnghia112@gmail.com>
2017-02-07 13:17:02 +0700

    [update] angular 2.4 - fix build prod

--- src/client/zone/media/favourites/favourites-list.component.ts
+++ src/client/zone/media/favourites/favourites-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { ZMediaFavoriteService } from './favourites.service';
+import { ZMediaPhotoDetailComponent } from '../photo/photo-detail.component';
+import { ConfirmationService } from 'primeng/components/common/api';
+import { LoadingService } from '../../../core/partials/loading/loading.service';
+import { ZMediaPhotoService } from '../photo/photo.service';
 
-// import { LoadingService, ConfirmationService } from '../../../shared/index';
-
-// declare var $: any;
+declare var $: any;
 declare var _: any;
 
 @Component({
@@ -12,15 +14,30 @@ declare var _: any;
   templateUrl: 'favourites-list.component.html'
 })
 export class ZMediaFavoriteListComponent implements OnInit {
+  @ViewChild('photoDetail') photoDetail: ZMediaPhotoDetailComponent;
+
   data: any = [];
   nextLink: string = null;
 
   selectedPhotos: any = [];
 
+  keyCtrl: boolean = false;
   hasFavourite: boolean = false;
   currentView: string = 'grid';
 
-  constructor(private favoriteService: ZMediaFavoriteService) {
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(ev: KeyboardEvent) {
+    console.log(ev);
+    if (ev.keyCode == 17 || ev.keyCode == 18 || ev.keyCode == 91 || ev.keyCode == 93 || ev.ctrlKey) this.keyCtrl = true;
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  onKeyUp(ev: KeyboardEvent) {
+    if (ev.keyCode == 17 || ev.keyCode == 18 || ev.keyCode == 91 || ev.keyCode == 93 || ev.ctrlKey) this.keyCtrl = false;
+  }
+
+  constructor(private photoService: ZMediaPhotoService,
+              private favoriteService: ZMediaFavoriteService) {
   }
 
   ngOnInit() {
@@ -41,6 +58,23 @@ export class ZMediaFavoriteListComponent implements OnInit {
   }
 
 
+  actionItem(event: any) {
+    //console.log(event);
+    switch (event.action) {
+      case 'select':
+        this.onSelectedPhotos(event.data);
+        break;
+      case 'previewAll':
+        this.onPreviewAll(event.data);
+        break;
+      case 'favourite':
+        this.onOneFavourite(event.data);
+        break;
+      default:
+        break;
+    }
+  }
+
   actionToolbar(event: any) {
     // console.log(event);
     switch (event) {
@@ -54,4 +88,42 @@ export class ZMediaFavoriteListComponent implements OnInit {
         break;
     }
   }
+
+  // --- Action for Item --- //
+  private onSelectedPhotos(item: any) {
+    if (this.keyCtrl) {
+      if (_.some(this.selectedPhotos, ['id', item.id])) {
+        $('#photo-box-img-' + item.id).removeClass('selected');
+        _.remove(this.selectedPhotos, ['id', item.id]);
+      } else {
+        $('#photo-box-img-' + item.id).addClass('selected');
+        this.selectedPhotos.push(item);
+      }
+    } else {
+      $('.row-img .photo-box-img').removeClass('selected');
+      $('#photo-box-img-' + item.id).addClass('selected');
+      this.selectedPhotos.length = 0;
+      this.selectedPhotos.push(item);
+      console.log(this.selectedPhotos);
+    }
+    this.hasFavourite = _.some(this.selectedPhotos, ['favorite', false]);
+  }
+
+  private onPreviewAll(item: any) {
+    this.photoDetail.selectedPhotos = this.photoDetail.allPhotos;
+    this.photoDetail.index = _.findIndex(this.photoDetail.allPhotos, ['id', item.id]);
+    this.photoDetail.preview(true);
+  }
+
+  private onOneFavourite(item: any) {
+    let findItemFavourite = _.findIndex(this.data, ['id', item.id]);
+    this.photoService.actionOneFavourite(item).subscribe((res: any)=> {
+      if (res.message === 'success') {
+        this.data[findItemFavourite].favorite = (this.data[findItemFavourite].favorite) ? false : true;
+      }
+    });
+  }
+
+  // --- End Action for Item --- //
+
 }
--- src/client/zone/media/shared-with-me/shared-with-me.module.ts
+++ src/client/zone/media/shared-with-me/shared-with-me.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { ZMediaSharedModule } from '../shared/shared.module';
 import { ZMediaSharedWithMeComponent } from './shared-with-me.component';
 import { ZMediaSharedWithMeService } from './shared-with-me.service';
+import { ZMediaPhotoModule } from '../photo/photo.module';
 
 @NgModule({
   imports: [
+    ZMediaPhotoModule,
     ZMediaSharedModule.forRoot()
   ],
   declarations: [
