816cdaa5ec36ba34c1a05ef4401dd27f0b11d3ad
trungnghia112 <trungnghia112@gmail.com>
2017-02-08 17:55:18 +0700

    [update] angular 2.4 - add base app project

---
+++ src/client/base/home/home-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: '', redirectTo: '/base', pathMatch: 'full' }
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule {
+}
