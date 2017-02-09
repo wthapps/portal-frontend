8068b80333a6c218da01c85b422c7e52f15d654a
trungnghia112 <trungnghia112@gmail.com>
2017-02-09 18:19:31 +0700

    [update] angular 2.4 - zone media add drag selectable

--- src/client/media/shared/list/list.component.ts
+++ src/client/media/shared/list/list.component.ts
@@ -1,17 +1,53 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
+
+declare var Selectables: any;
 
 @Component({
   moduleId: module.id,
   selector: 'z-media-share-list',
   templateUrl: 'list.component.html'
 })
-export class ZMediaShareListComponent {
+export class ZMediaShareListComponent implements AfterViewInit {
   @Input() type: string = '';
   @Input() data: any = [];
   @Input() view: string = 'grid';
   @Input() hasAction: any = []; // favourite, select, preview, previewAll
 
   @Output() outEvent: EventEmitter<any> = new EventEmitter<any>();
+
+
+  selectables: any;
+  selectablesEnable: boolean = false;
+
+  ngAfterViewInit() {
+    this.selectables = new Selectables({
+      zone: '#list-photo',
+      elements: 'z-media-share-item',
+      selectedClass: 'active',
+      onSelect: (element: any)=> {
+        console.log(element);
+      }
+    });
+
+    // //later
+    this.selectables.disable();
+    //
+    // // enable again
+    // dr.enable();
+
+  }
+
+  onDragenter(e: any) {
+    e.preventDefault();
+    if (!this.selectablesEnable) {
+      this.selectables.enable();
+    }
+  }
+
+  onDragleave(e: any) {
+    e.preventDefault();
+    this.selectablesEnable = false;
+  }
 
   actionItem(ev: any) {
     this.outEvent.emit(ev);
