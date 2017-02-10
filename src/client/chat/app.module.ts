a9fb2d6148951ed2d9bd6d9082bc4d9bea71eb8d
trungnghia112 <trungnghia112@gmail.com>
2017-02-10 17:55:14 +0700

    [update] angular 2.4 - zone chat

---
+++ src/client/chat/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { SharedModule } from '../core/shared/shared.module';
+import { ChatSharedModule } from './shared/shared.module';
+import { ZChatSettingModule } from './setting/setting.module';
+import { ZChatConversationModule } from './conversation/conversation.module';
+import { ZChatContactModule } from './contact/contact.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule,
+    ChatSharedModule.forRoot(),
+    SharedModule.forRoot(),
+    ZChatSettingModule,
+    ZChatConversationModule,
+    ZChatContactModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [{
+    provide: APP_BASE_HREF,
+    useValue: '<%= APP_BASE %>'
+  }],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule {
+}
