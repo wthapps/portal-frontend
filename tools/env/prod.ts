4efb1a185afdd1a860228f5e8f293c774d5d8919
trungnghia112 <trungnghia112@gmail.com>
2017-02-22 17:41:35 +0700

    [update] angular 2.4 - deploy prod

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -1,15 +1,15 @@
 import { EnvConfig } from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
-  API: 'http://localhost:4000',
+  API: 'http://54.213.41.54:4000',
   ENV: 'PROD',
-  DOMAIN: 'localhost',
+  DOMAIN: '54.213.41.54',
   SUB_DOMAIN: {
-    APP: 'https://localhost:3000',
-    MYACCOUNT: 'https://localhost:3100',
-    MEDIA: 'https://localhost:3200',
-    SOCIAL: 'https://localhost:3300',
-    CHAT: 'https://localhost:3400'
+    APP: 'http://54.213.41.54:3000',
+    MYACCOUNT: 'http://54.213.41.54:3005',
+    MEDIA: 'http://54.213.41.54:3010',
+    SOCIAL: 'http://54.213.41.54:3015',
+    CHAT: 'http://54.213.41.54:3020'
   },
   RES: 'https://s3-ap-southeast-1.amazonaws.com/env-staging'
 };
