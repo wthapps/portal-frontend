58bacc5af659aa843cefbac56a4543e1d816c70a
trungnghia112 <trungnghia112@gmail.com>
2017-03-16 13:36:38 +0700

    [update] fix build prod

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -3,7 +3,8 @@ import { EnvConfig } from './env-config.interface';
 const DevConfig: EnvConfig = {
   // API: 'http://54.213.41.54:4000',
   // API: 'http://192.168.0.105:4000',
-  API: 'http://localhost:4000',
+  // API: 'http://localhost:4000',
+  API: 'http://192.168.0.106:4000',
   ENV: 'DEV',
   DOMAIN: 'localhost',
   SUB_DOMAIN: {
