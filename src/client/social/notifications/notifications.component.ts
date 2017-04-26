d6e8fa9f2ab7cddaad7434750f3ed9b09d40fc64
David Nguyen <david@wthapps.com>
2017-04-26 11:38:10 +0700

    social-edit-notification

--- src/client/social/notifications/notifications.component.ts
+++ src/client/social/notifications/notifications.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SocialService } from '../shared/services/social.service';
-import { ApiBaseService } from '../../core/shared/services/apibase.service';
+// import { ApiBaseService } from '../../core/shared/services/apibase.service';
 import { Constants } from '../../core/shared/config/constants';
 import { NotificationService } from '../../core/shared/services/notification.service';
-import { SocialDataService } from '../shared/services/social-data.service';
+// import { SocialDataService } from '../shared/services/social-data.service';
 import { Subscription } from 'rxjs';
 // import { SocialService } from '../services/social.service';
 // import { ApiBaseService } from '../../../shared/services/apibase.service';
@@ -22,48 +22,21 @@ export class ZSocialNotificationsComponent implements OnInit, OnDestroy {
   readonly communitiesUrl: string = '/' + Constants.urls.communities;
   readonly profileUrl: string = '/'+Constants.urls.profile;
 
-  // Subscription loading
-  loadSubscription : Subscription;
-
   constructor(private socialService: SocialService,
-              private socialDataService : SocialDataService,
               public notificationService: NotificationService) {
   }
 
   ngOnInit() {
-    // this.callNotifications();
+    this.callNotifications();
+  }
 
-    this.socialDataService.resetLoading();
-    this.loadSubscription = this.socialDataService.itemObs$.subscribe( () => {
-        this.getMoreNotifications();
+  callNotifications() {
+    this.socialService.user.getNotifications().subscribe(
+      (res: any) => {
+        this.notifications = res.data;
+        this.newNotifications = _.filter(this.notifications, {'seen_state': 'new'});
       }
     );
-  }
-
-  ngOnDestroy() {
-    this.loadSubscription.unsubscribe();
-  }
-
-  // callNotifications() {
-  //   this.socialService.user.getNotifications().subscribe(
-  //     (res: any) => {
-  //       console.log(res.data);
-  //       this.notifications = res.data;
-  //       this.newNotifications = _.filter(this.notifications, {'seen_state': 'new'});
-  //     }
-  //   );
-  // }
-
-  getMoreNotifications() {
-    // if (!this.loadingDone())
-      this.notificationService.getMoreNotifications();
-  }
-
-  loadingDone() {
-    return this.notificationService.loadingDone;  }
-
-  notifClass(notif: any) {
-    return 'col-xs-12 col-lg-6 ' + (!notif.is_read ? 'unread-notification' : '');
   }
 
   doAction(action: any, notif_id: string) {
@@ -84,5 +57,9 @@ export class ZSocialNotificationsComponent implements OnInit, OnDestroy {
     // );
 
     this.notificationService.doAction(action, notif_id);
+  }
+
+  getMoreNotifications() {
+
   }
 }
