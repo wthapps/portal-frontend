e9a1aba475d046dfa0de80a0ec4f2e63e26b22e1
trungnghia112 <trungnghia112@gmail.com>
2017-05-29 13:21:39 +0700

    [chat] - Apply new UI that Ray finished to all pages of Contacts
    refs #WTHZONE-219

--- src/client/chat/contact/contact-receive.component.ts
+++ src/client/chat/contact/contact-receive.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChatService } from '../shared/services/chat.service';
 import { ZChatShareRequestContactComponent } from '../shared/modal/request-contact.component';
+import { ConfirmationService } from 'primeng/components/common/api';
 
 @Component({
   moduleId: module.id,
@@ -8,20 +9,30 @@ import { ZChatShareRequestContactComponent } from '../shared/modal/request-conta
   templateUrl: 'contact-receive.component.html'
 })
 export class ZChatContactReceiveComponent implements OnInit {
-  users:any;
+  users: any;
   @ViewChild('request') requestModal: ZChatShareRequestContactComponent;
 
-  constructor(private chatService: ChatService) {
+  constructor(private chatService: ChatService, private confirmationService: ConfirmationService) {
   }
 
   ngOnInit() {
-    this.chatService.apiBaseService.post('zone/chat/contact/get_share_contacts').subscribe((res:any) => {
+    this.chatService.apiBaseService.post('zone/chat/contact/get_share_contacts').subscribe((res: any) => {
       this.users = res.data;
     });
   }
 
-  onAddToContact(contact:any) {
+  onAddToContact(contact: any) {
     this.requestModal.contact = contact;
     this.requestModal.modal.open();
+  }
+
+  addBlackList(contact: any) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to add this contact to black list ?',
+      header: 'Add To Black List',
+      accept: () => {
+        this.chatService.addGroupUserBlackList(contact);
+      }
+    });
   }
 }
