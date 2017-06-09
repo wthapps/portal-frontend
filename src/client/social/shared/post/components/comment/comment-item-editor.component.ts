7aedb25741bc317dd133bc88482d553bc3731fbe
An Vo <albert@wthapps.com>
2017-06-09 18:15:15 +0700

    [SOCIAL] Post - Comment + Fix cancel edit comment editor case + Remove JQuery reference in comments

--- src/client/social/shared/post/components/comment/comment-item-editor.component.ts
+++ src/client/social/shared/post/components/comment/comment-item-editor.component.ts
@@ -34,12 +34,12 @@ export class CommentItemEditorComponent implements OnInit {
   // @Input() item: SoPost;
   @Input() parent: any; // parent is able to be Post or Comment or Photo or other object
   @Input() parentType: string = 'SocialNetwork::Post';  // 'SocialNetwork::Post' or 'SocialNetwork::Comment'
-
-  @Input() comment: SoComment = new SoComment();
+  @Input() originComment = new SoComment();
   @Input() reply: SoComment;
   @Input() mode: any = CommentEditorMode.Add;
   @Output() eventEmitter: EventEmitter<any> = new EventEmitter<any>();
 
+  comment: SoComment = new SoComment(); // Clone comment
   commentEditorMode = CommentEditorMode;
   hasUploadingPhoto: boolean = false;
   files: any;
@@ -58,6 +58,8 @@ export class CommentItemEditorComponent implements OnInit {
     if (this.mode == CommentEditorMode.Add) {
       // this.comment = new SoComment();
     }
+
+    this.comment = _.cloneDeep(this.originComment);
     //Init From controls
     this.commentEditorForm = this.fb.group({
       'content': [this.comment.content, null],
@@ -158,19 +160,23 @@ export class CommentItemEditorComponent implements OnInit {
     // if (this.type == this.commentEditorMode.EditReply) {
     //   this.eventEmitter.emit(new CancelEditReplyCommentEvent(this.reply));
     // }
+
+
+    this.comment.content = '';
+    this.comment.photo = null;
+    this.commentEditorForm.value.content = '';
+    this.commentEditorForm.value.photo = null;
     if (this.mode == CommentEditorMode.Add) {
       // add new comment/reply to post
       console.log('canceling add...........', this.comment);
-      this.comment.content = '';
-      this.comment.photo = null;
-      this.commentEditorForm.value.content = '';
-      this.commentEditorForm.value.photo = null;
 
+      _.set(this.originComment, 'isCreatingNewReply', false);
       // this.eventEmitter.emit(new CancelAddCommentEvent(this.comment));
 
     } else if(this.mode == CommentEditorMode.Edit) {
       // update current comment/reply
       console.log('canceling edit...........');
+      _.set(this.originComment, 'isEditting', false);
       this.eventEmitter.emit(new CancelEditCommentEvent(this.comment));
     }
 
@@ -187,6 +193,7 @@ export class CommentItemEditorComponent implements OnInit {
       this.comment.parentId = this.parent.uuid;
       this.comment.parentType = this.parentType;
 
+      _.set(this.originComment, 'isCreatingNewReply', false);
       event = new CommentCreateEvent(this.comment);
 
     } else if(this.mode == CommentEditorMode.Edit) {
