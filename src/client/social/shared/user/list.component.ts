e4ed24ba9550d11b7216e3f2e21239e42868dfd4
trungnghia112 <trungnghia112@gmail.com>
2017-06-16 14:31:35 +0700

    [social] - modify - change html structure 'z-social-box' to 'panel'

--- src/client/social/shared/user/list.component.ts
+++ src/client/social/shared/user/list.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit, HostBinding, Input } from '@angular/core';
+/**
+ *
+ */
+
+import { Component, OnInit, HostBinding, Input, OnChanges } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'z-social-share-profile-list',
   templateUrl: 'list.component.html'
 })
-export class ZSocialShareProfileListComponent implements OnInit {
+export class ZSocialShareProfileListComponent implements OnInit, OnChanges {
   @Input() data: any;
   @Input() type: string;
+  @Input() layout: string = 'list-group'; //row
+  @Input() class: string = 'list-group-item'; //col-xs-6
 
-  @HostBinding('class') class = 'list-group';
+  @HostBinding('class') classHost = this.layout;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.layout == 'row') {
+      this.classHost = this.layout + ' row-striped';
+    }
   }
 }
