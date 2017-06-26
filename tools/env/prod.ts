47bde6bf0ad081047a0fb7cd5b9cc4c14276d7d4
Thinh Huynh <hdthinh@gmail.com>
2017-06-26 16:18:59 +0700

    configuration-apply DNS

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -2,23 +2,40 @@ import { EnvConfig } from './env-config.interface';
 import { PORTS } from '../../src/client/core/shared/config/build-env';
 
 // var hostname = '192.168.0.108';
-var hostname = '54.213.41.54';
-
 var protocol = 'https';
+var domain = 'wthapps.com';
+var subdomain = `testing`;
+var fullDomain = `${subdomain}.${domain}`;
+
+
 
 const ProdConfig: EnvConfig = {
-  API:`${protocol}://${hostname}:${PORTS['api']}`,
+  API:`${protocol}://api-${fullDomain}`,
   ENV: 'PROD',
-  DOMAIN: `${hostname}`,
+  DOMAIN: `${fullDomain}`,
   SUB_DOMAIN: {
-    APP: `${protocol}://${hostname}:${PORTS['portal']}`,
-    MYACCOUNT: `${protocol}://${hostname}:${PORTS['my']}`,
-    MEDIA: `${protocol}://${hostname}:${PORTS['media']}`,
-    SOCIAL: `${protocol}://${hostname}:${PORTS['social']}`,
-    CHAT: `${protocol}://${hostname}:${PORTS['chat']}`
+    APP: `${protocol}://${fullDomain}`,
+    MYACCOUNT: `${protocol}://my-${fullDomain}`,
+    MEDIA: `${protocol}://media-${fullDomain}`,
+    SOCIAL: `${protocol}://social-${fullDomain}`,
+    CHAT: `${protocol}://chat-${fullDomain}`
   },
   RES: 'https://s3-us-west-2.amazonaws.com/env-staging-oregon'
 };
+
+// const ProdConfig: EnvConfig = {
+//   API:`${protocol}://${hostname}:${PORTS['api']}`,
+//   ENV: 'PROD',
+//   DOMAIN: `${hostname}`,
+//   SUB_DOMAIN: {
+//     APP: `${protocol}://${hostname}:${PORTS['portal']}`,
+//     MYACCOUNT: `${protocol}://${hostname}:${PORTS['my']}`,
+//     MEDIA: `${protocol}://${hostname}:${PORTS['media']}`,
+//     SOCIAL: `${protocol}://${hostname}:${PORTS['social']}`,
+//     CHAT: `${protocol}://${hostname}:${PORTS['chat']}`
+//   },
+//   RES: 'https://s3-us-west-2.amazonaws.com/env-staging-oregon'
+// };
 
 export = ProdConfig;
 
