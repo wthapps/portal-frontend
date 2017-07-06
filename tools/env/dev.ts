8625ad2a39a6730c8c648a273e656f4bcf22fd30
David Nguyen <david@wthapps.com>
2017-07-06 14:17:52 +0700

    update: dev.ts ref N/A

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -3,9 +3,9 @@ import { EnvConfig } from './env-config.interface';
 const DevConfig: EnvConfig = {
   // API: 'http://54.213.41.54:4000',
   // API: 'http://192.168.0.105:4000',
-  // API: 'http://localhost:4000',
+  API: 'http://localhost:4000',
   // API: 'http://192.168.0.109:4000',
-  API: 'http://192.168.0.103:4000',
+  // API: 'http://192.168.0.103:4000',
   ENV: 'DEV',
   DOMAIN: 'localhost',
   SUB_DOMAIN: {
