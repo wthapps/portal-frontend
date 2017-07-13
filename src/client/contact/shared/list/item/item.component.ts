c1049459afc0df2a5d191b409c7c882e1009395b
trungnghia112 <trungnghia112@gmail.com>
2017-07-13 14:16:43 +0700

    update: make layout ui contact group menu ref WTHZONE-294

--- src/client/contact/shared/list/item/item.component.ts
+++ src/client/contact/shared/list/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Constants } from '../../../../core/shared/config/constants';
 import { CountryService } from '../../../../core/partials/countries/countries.service';
 import { ZContactService } from '../../services/contact.service';
@@ -10,6 +10,7 @@ import { ZContactService } from '../../services/contact.service';
 })
 export class ZContactSharedItemComponent implements OnInit {
   @Input() data: any;
+  @HostBinding('class') cssClass:string = 'contact-listbox-row';
 
   selected: boolean = false;
 
@@ -35,8 +36,10 @@ export class ZContactSharedItemComponent implements OnInit {
     this.selected = !this.selected;
     if (this.selected) {
       this.contactService.addItemSelectedObjects(this.data);
+      this.cssClass = 'contact-listbox-row active';
     } else {
       this.contactService.removeItemSelectedObjects(this.data);
+      this.cssClass = 'contact-listbox-row';
     }
   }
 }
