d08492ada88983d64d413369ec8fd43297b5af60
trungnghia112 <trungnghia112@gmail.com>
2017-08-07 16:56:06 +0700

    update: media core share module

--- src/client/media/shared/shared.module.ts
+++ src/client/media/shared/shared.module.ts
@@ -1,6 +1,4 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { MediaUploaderComponent } from './uploader/media-uploader.component';
 import { MediaViewContainerComponent } from './container/media-view-container.comoponent';
 import { MediaToolbarListComponent } from './media/media-toolbar-list.component';
@@ -9,13 +7,10 @@ import { MediaItemComponent } from './media/media-item.component';
 import { ZMediaAlbumService } from '../album/album.service';
 import { MediaUploaderDataService } from './uploader/media-uploader-data.service';
 
-import { ModalDockModule } from '../../core/shared/components/modal/dock.module';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { PartialsPhotoModule } from '../../core/shared/components/photo/photo.module';
-import { PipeModule } from '../../core/shared/pipe/pipe.module';
 import { ZMediaSharingService } from '../../core/shared/components/photo/modal/sharing/sharing.service';
 import { ZMediaTaggingService } from '../../core/shared/components/photo/modal/tagging/tagging.service';
 import { ZMediaStore } from './store/media.store';
+import { CoreSharedModule } from '../../core/shared/shared.module';
 
 
 /**
@@ -24,12 +19,7 @@ import { ZMediaStore } from './store/media.store';
 
 @NgModule({
   imports: [
-    CommonModule,
-    RouterModule,
-    ModalDockModule,
-    InfiniteScrollModule,
-    PartialsPhotoModule,
-    PipeModule
+    CoreSharedModule.forRoot()
   ],
   declarations: [
     MediaUploaderComponent,
