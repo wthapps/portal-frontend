a9ce3c17bf3902e50b57d81ca03a73b496b93b38
trungnghia112 <trungnghia112@gmail.com>
2017-08-28 11:42:19 +0700

    modify: pricing page ref WTHZONE-379

--- src/client/portal/pricing/pricing.component.ts
+++ src/client/portal/pricing/pricing.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { fadeInAnimation } from '../../core/shared/animations/route.animation';
 
 @Component({
   moduleId: module.id,
   selector: 'sd-pricing',
-  templateUrl: 'pricing.component.html'
+  templateUrl: 'pricing.component.html',
+  styleUrls: ['pricing.component.css'],
+  host: {
+    "[@fadeInAnimation]": 'true'
+  },
+  animations: [fadeInAnimation]
 })
 
 export class PricingComponent {
