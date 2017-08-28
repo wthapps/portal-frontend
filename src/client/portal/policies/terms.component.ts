602673b59b5da91bebf8f2f282640ba2df72340a
trungnghia112 <trungnghia112@gmail.com>
2017-08-28 17:42:58 +0700

    modify: terms of service page page ref WTHZONE-380

--- src/client/portal/policies/terms.component.ts
+++ src/client/portal/policies/terms.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'sd-terms',
-  templateUrl: 'terms.component.html'
+  templateUrl: 'terms.component.html',
+  styleUrls: ['terms.component.css']
 })
 
 export class TermsComponent {
