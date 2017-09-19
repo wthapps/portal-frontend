b11a749aaf4e45f774147782faf8f064bfd54171
David Nguyen <david@wthapps.com>
2017-09-19 14:43:13 +0700

    add: note call api

--- src/client/note/my-note/my-note.component.ts
+++ src/client/note/my-note/my-note.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ZNoteService } from '../shared/services/note.service';
 import { Note } from '../../core/shared/models/note.model';
+import { ApiBaseService } from '../../core/shared/services/apibase.service';
 
 declare var _: any;
 
@@ -12,17 +13,20 @@ declare var _: any;
 export class ZNoteMyNoteComponent implements OnInit {
   data: Note[] = [];
 
-  constructor(private noteService: ZNoteService) {
+  constructor(private noteService: ZNoteService, private apiBaseService: ApiBaseService) {
     this.noteService.notes$.subscribe((res: any)=> {
       this.data = res.data;
     });
   }
 
   ngOnInit() {
-    this.noteService.getList().subscribe(
-      res => {
-        console.log(res);
-      }
-    );
+    // this.noteService.getList().subscribe(
+    //   res => {
+    //     console.log(res);
+    //   }
+    // );
+    this.apiBaseService.get(`note/folder`).subscribe((res: any) => {
+      console.log(res)
+    })
   }
 }
