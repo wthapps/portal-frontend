da264a4fa32b66f5f7a5d077bae6d3ce4d15e3c4
Thinh Huynh <hdthinh@gmail.com>
2017-12-14 18:07:26 +0700

    social-fix missing import and change pricing route

--- src/social/search/all/search-all.component.ts
+++ src/social/search/all/search-all.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ServiceManager } from '@wth/shared/services';
 
