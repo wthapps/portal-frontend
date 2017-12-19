5fc7c12870dd75bc525841470128e7de774c4379
Thinh Huynh <hdthinh@gmail.com>
2017-12-19 16:54:07 +0700

    portal-update build prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,47 +3,47 @@ export const environment = {
   production: true
 };
 
-
-// import { PORTS } from '../../src/client/core/shared/config/build-env';
-
-// var hostname = '192.168.0.108';
-// var protocol = 'https';
-// var domain = 'wthapps.com';
-// var subdomain = ``;
-// var fullDomain = `${subdomain}.${domain}`;
-//
-// export const ConfigByEnv: EnvConfig = {
-//   API:`${protocol}://api${fullDomain}`,
-//   ENV: 'PROD',
-//   DOMAIN: `${domain}`,
-//   SUB_DOMAIN: {
-//     APP: `${protocol}://www${fullDomain}`,
-//     MYACCOUNT: `${protocol}://myaccount${fullDomain}`,
-//     MEDIA: `${protocol}://media${fullDomain}`,
-//     SOCIAL: `${protocol}://social${fullDomain}`,
-//     CHAT: `${protocol}://chat${fullDomain}`,
-//     CONTACT: `${protocol}://contacts${fullDomain}`,
-//     NOTE: `${protocol}://notes${fullDomain}`,
-//   }
-// };
-
 console.log('environment:::', environment);
 
+// var hostname = '192.168.0.108';
+var protocol = 'https';
+var domain = 'wthapps.com';
+var subdomain = ``;
+var fullDomain = `${subdomain}.${domain}`;
+
 export const ConfigByEnv: EnvConfig = {
-  // API: 'http://54.213.41.54:4000',
-  API: 'http://localhost:4000',
-  // API: 'http://192.168.0.106:4000',
-  // API: 'http://192.168.0.111:4000',
-  // API: 'http://192.168.0.103:4000',
+  CDN: `${protocol}://cdn-apps.${domain}`,
+  API:`${protocol}://api${fullDomain}`,
   ENV: 'PROD',
-  DOMAIN: 'localhost',
+  DOMAIN: `${domain}`,
   SUB_DOMAIN: {
-    APP: 'http://localhost:3000',
-    MYACCOUNT: 'http://localhost:3005',
-    MEDIA: 'http://localhost:3010',
-    SOCIAL: 'http://localhost:3015',
-    CHAT: 'http://localhost:3020',
-    CONTACT: 'http://localhost:3025',
-    NOTE: 'http://localhost:3030',
+    APP: `${protocol}://www${fullDomain}`,
+    MYACCOUNT: `${protocol}://myaccount${fullDomain}`,
+    MEDIA: `${protocol}://media${fullDomain}`,
+    SOCIAL: `${protocol}://social${fullDomain}`,
+    CHAT: `${protocol}://chat${fullDomain}`,
+    CONTACT: `${protocol}://contacts${fullDomain}`,
+    NOTE: `${protocol}://notes${fullDomain}`,
   }
 };
+
+// console.log('environment:::', environment);
+//
+// export const ConfigByEnv: EnvConfig = {
+//   // API: 'http://54.213.41.54:4000',
+//   API: 'http://localhost:4000',
+//   // API: 'http://192.168.0.106:4000',
+//   // API: 'http://192.168.0.111:4000',
+//   // API: 'http://192.168.0.103:4000',
+//   ENV: 'PROD',
+//   DOMAIN: 'localhost',
+//   SUB_DOMAIN: {
+//     APP: 'http://localhost:3000',
+//     MYACCOUNT: 'http://localhost:3005',
+//     MEDIA: 'http://localhost:3010',
+//     SOCIAL: 'http://localhost:3015',
+//     CHAT: 'http://localhost:3020',
+//     CONTACT: 'http://localhost:3025',
+//     NOTE: 'http://localhost:3030',
+//   }
+// };
