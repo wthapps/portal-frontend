5b6b58c8984548c6e96271012639ec3e5e15ff5a
Thinh Huynh <hdthinh@gmail.com>
2018-01-19 18:56:18 +0700

    note-disable selection to fix runtime bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,46 +4,46 @@ export const environment = {
   production: true
 };
 
-// console.log('environment:::', environment);
-//
-// // var hostname = '192.168.0.108';
-// const protocol = 'https';
-// const domain = 'wthapps.com';
-// const subdomain = ``;
-// const fullDomain = `${subdomain}.${domain}`;
-//
-// export const ConfigByEnv: EnvConfig = {
-//   CDN: `${protocol}://cdn-apps.${domain}`,
-//   API: `${protocol}://api${fullDomain}`,
-//   ENV: 'PROD',
-//   DOMAIN: `${domain}`,
-//   SUB_DOMAIN: {
-//     APP: `${protocol}://www${fullDomain}`,
-//     MYACCOUNT: `${protocol}://myaccount${fullDomain}`,
-//     MEDIA: `${protocol}://media${fullDomain}`,
-//     SOCIAL: `${protocol}://social${fullDomain}`,
-//     CHAT: `${protocol}://chat${fullDomain}`,
-//     CONTACT: `${protocol}://contacts${fullDomain}`,
-//     NOTE: `${protocol}://notes${fullDomain}`,
-//   }
-// };
+console.log('environment:::', environment);
 
-
-// const host = '192.168.0.116';
-const host = 'localhost';
+// var hostname = '192.168.0.108';
+const protocol = 'https';
+const domain = 'wthapps.com';
+const subdomain = ``;
+const fullDomain = `${subdomain}.${domain}`;
 
 export const ConfigByEnv: EnvConfig = {
-  CDN: `http://${host}:4000/assets`,
-  API: `http://${host}:4000`,
-  ENV: 'DEV',
-  DOMAIN: 'localhost',
+  CDN: `${protocol}://cdn-apps.${domain}`,
+  API: `${protocol}://api${fullDomain}`,
+  ENV: 'PROD',
+  DOMAIN: `${domain}`,
   SUB_DOMAIN: {
-    APP: 'http://localhost:3000',
-    MYACCOUNT: 'http://localhost:3005',
-    MEDIA: 'http://localhost:3010',
-    SOCIAL: 'http://localhost:3015',
-    CHAT: 'http://localhost:3020',
-    CONTACT: 'http://localhost:3025',
-    NOTE: 'http://localhost:3030',
+    APP: `${protocol}://www${fullDomain}`,
+    MYACCOUNT: `${protocol}://myaccount${fullDomain}`,
+    MEDIA: `${protocol}://media${fullDomain}`,
+    SOCIAL: `${protocol}://social${fullDomain}`,
+    CHAT: `${protocol}://chat${fullDomain}`,
+    CONTACT: `${protocol}://contacts${fullDomain}`,
+    NOTE: `${protocol}://notes${fullDomain}`,
   }
 };
+
+
+// // const host = '192.168.0.116';
+// const host = 'localhost';
+//
+// export const ConfigByEnv: EnvConfig = {
+//   CDN: `http://${host}:4000/assets`,
+//   API: `http://${host}:4000`,
+//   ENV: 'DEV',
+//   DOMAIN: 'localhost',
+//   SUB_DOMAIN: {
+//     APP: 'http://localhost:3000',
+//     MYACCOUNT: 'http://localhost:3005',
+//     MEDIA: 'http://localhost:3010',
+//     SOCIAL: 'http://localhost:3015',
+//     CHAT: 'http://localhost:3020',
+//     CONTACT: 'http://localhost:3025',
+//     NOTE: 'http://localhost:3030',
+//   }
+// };
