43bf8420df7af940ee08a351eb87ad5f8e86eef4
trungnghia112 <trungnghia112@gmail.com>
2018-05-23 13:11:39 +0700

    update: contact - add validation for input data #WTHZONE-703

--- src/shared/shared/validator/custom.validator.ts
+++ src/shared/shared/validator/custom.validator.ts
@@ -11,8 +11,8 @@ export class CustomValidator {
     }
 
     // tslint:disable-next-line
-    let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-    return EMAIL_REGEXP.test(c.value) ? null : { emailFormat: true };
+    let EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return EMAIL_REGEXP.test(c.value) ? null : {emailFormat: true};
   }
 
   public static urlFormat(c: AbstractControl) {
