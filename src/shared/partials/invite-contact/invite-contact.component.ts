7811d0cdf14f8dbfa3ce35ddbb1d46ab54c253b0
David Nguyen <david@wthapps.com>
2018-10-01 17:24:23 +0700

    update: easy invite email

--- src/shared/partials/invite-contact/invite-contact.component.ts
+++ src/shared/partials/invite-contact/invite-contact.component.ts
@@ -1,18 +1,81 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Constants } from '@shared/constant';
 import { BsModalComponent } from 'ng2-bs3-modal';
+import { ApiBaseService } from '@shared/services';
+import { InvitationCreateModalComponent } from '@shared/shared/components/invitation/invitation-create-modal.component';
 
 @Component({
   selector: 'invite-contact',
   templateUrl: './invite-contact.component.html',
-  styleUrls: ['./invite-contact.component.css']
+  styleUrls: ['./invite-contact.component.scss']
 })
 export class InviteContactComponent implements OnInit {
   @ViewChild('modal') modal: BsModalComponent;
+  @ViewChild('create') createModal: InvitationCreateModalComponent;
+  contacts: any;
+  suggestedUsers: any = [];
+  selectedUsers: any = [];
 
+  constructor(private apiBaseService: ApiBaseService) {}
   ngOnInit() {}
 
   openModal() {
+    this.apiBaseService.get(`contact/contacts`).subscribe(res => {
+      this.contacts = res.data.filter(e => !e.wthapps_user);
+    })
     this.modal.open();
+  }
+
+  openCreate(e: any) {
+    this.modal.close();
+    let data = [];
+    this.contacts.filter(c => c.selected).forEach(c => {
+      if(c.emails && c.emails.length > 0) {
+        c.emails.forEach(e => {
+          data.push({ fullName: c.name, contactId: c.id, email: e.value });
+        })
+      } else {
+        data.push({ fullName: c.name, contactId: c.id, email: ""});
+      }
+    });
+    this.createModal.open({data: data});
+  }
+
+  selectUser(e: any) {
+    e.selected = true;
+    this.contacts = this.contacts.map(c => {
+      if (c.id == e.id) {
+        return e;
+      }
+      return c;
+    })
+  }
+
+  deselectUser(e: any){
+    e.selected = false;
+    this.contacts = this.contacts.map(c => {
+      if(c.id == e.id) {
+        return e;
+      }
+      return c;
+    })
+  }
+
+  onCheckboxChange(user){
+    if (user.selected) {
+      this.selectedUsers.push(user);
+    } else {
+      this.selectedUsers = this.selectedUsers.filter(u => u.id !== user.id);
+    }
+  }
+
+  onSearch(e: any) {
+    var rgxp = new RegExp(e.query, "i");
+    this.suggestedUsers = this.contacts.filter(c => {
+      if (c.name.match(rgxp)){
+        return true;
+      }
+      return false;
+    });
   }
 }
--- src/shared/partials/invite-contact/invite-contact.module.ts
+++ src/shared/partials/invite-contact/invite-contact.module.ts
@@ -1,14 +1,14 @@
 import { InviteContactComponent } from '@shared/partials/invite-contact/invite-contact.component';
 import { NgModule } from '@angular/core';
 import { BsModalModule } from 'ng2-bs3-modal';
-import { TooltipModule } from 'primeng/primeng';
+import { TooltipModule, CheckboxModule } from 'primeng/primeng';
 import { TextboxSearchModule } from '@shared/partials/search-box/textbox-search.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AutoCompleteModule } from 'primeng/components/autocomplete/autocomplete';
-import { WthFilterByPipe } from '@shared/shared/pipe/wthFilterBy.pipe';
 import { PipeModule } from '@shared/shared/pipe/pipe.module';
 import { BoxLoadingModule } from '@shared/shared/components/box-loading/box-loading.module';
+import { InvitationModule } from '@shared/shared/components/invitation/invitation.module';
 
 @NgModule({
   imports: [
@@ -20,6 +20,8 @@ import { BoxLoadingModule } from '@shared/shared/components/box-loading/box-load
     AutoCompleteModule,
     PipeModule,
     BoxLoadingModule,
+    CheckboxModule,
+    InvitationModule,
     ReactiveFormsModule
   ],
   declarations: [InviteContactComponent],
