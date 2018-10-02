1839eabf831a297cf582921d1838e0fe45f10ce6
trungnghia112 <trungnghia112@gmail.com>
2018-10-02 17:41:45 +0700

    update: Chat - Update Contacts and conversation page #WTHZONE-753

--- src/chat/shared/user/user.component.ts
+++ src/chat/shared/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'z-chat-share-user',
@@ -8,6 +8,7 @@ export class ZChatShareUserComponent implements OnInit {
   @Input() data: any;
   @Input() size = 'xs'; // xs, xsm, sm, md, lg
   @Input() displayUserName = true;
+  @Input() displayAvatar: Boolean = true;
   @Input() usersOnlineItem: any;
   @Input() isStranger = false;
 
