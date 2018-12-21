7babb352afcfbd64fd3a8bbf2fb8b4c26e9a2206
trungnghia112 <trungnghia112@gmail.com>
2018-12-21 17:01:24 +0700

    update - Account - Update layout - profile #WTHZONE-1069

--- src/my-account/settings/profile/profile.component.ts
+++ src/my-account/settings/profile/profile.component.ts
@@ -1,25 +1,16 @@
-import { Component, OnInit, ViewChild, OnDestroy }    from '@angular/core';
-
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import {
-  FormGroup,
-  AbstractControl,
-  FormBuilder,
-  Validators
-} from '@angular/forms';
-
-import { CustomValidator } from '@wth/shared/shared/validator/custom.validator';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiBaseService, UrlService } from '@shared/services';
+import { Constants } from '@wth/shared/constant/config/constants';
+import { CommonEventService } from '@wth/shared/services/common-event/common-event.service';
 import { UserService } from '@wth/shared/services/user.service';
 import { CountryService } from '@wth/shared/shared/components/countries/countries.service';
-import { ToastsService } from '@wth/shared/shared/components/toast/toast-message.service';
 import { LoadingService } from '@wth/shared/shared/components/loading/loading.service';
-import { CommonEventService } from '@wth/shared/services/common-event/common-event.service';
-import { Constants } from '@wth/shared/constant/config/constants';
-import { ApiBaseService, UrlService } from '@shared/services';
+import { ToastsService } from '@wth/shared/shared/components/toast/toast-message.service';
 
-declare var $: any;
-declare var _: any;
+import { CustomValidator } from '@wth/shared/shared/validator/custom.validator';
+
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'my-setting-profile',
@@ -27,19 +18,12 @@ declare var _: any;
 })
 
 export class MyProfileComponent implements OnInit, OnDestroy {
-  // @ViewChild('uploadProfile') uploadProfile: UploadCropImageComponent;
-
-  pageTitle = 'Profile';
   errorMessage = Constants.errorMessage.default;
-  defaultAvatar = Constants.img.avatar;
   profile_image = '';
 
   sex = 0;
-  birthdayDate: any = {
-    day: 0,
-    month: 0,
-    year: 0
-  };
+
+  yearRange: string;
 
   formValue: any;
 
@@ -51,13 +35,8 @@ export class MyProfileComponent implements OnInit, OnDestroy {
   email: AbstractControl;
   phone_prefix: AbstractControl;
   phone_number: AbstractControl;
-  birthday_day: AbstractControl;
-  birthday_month: AbstractControl;
-  birthday_year: AbstractControl;
+  birthday: AbstractControl;
 
-  validDays: number[] = [];
-  // validMonths: number[] = [];
-  validYears: number[] = [];
   submitted = false;
   confirmedEmail: any = null;
   verified = false;
@@ -72,21 +51,20 @@ export class MyProfileComponent implements OnInit, OnDestroy {
               private urlService: UrlService,
               private commonEventService: CommonEventService,
               private loadingService: LoadingService) {
-
     this.sex = this.userService.getSyncProfile().sex === null ? 0 : this.userService.getSyncProfile().sex;
-    this.validDays = this.range(1, 31);
-    this.validYears = this.range(2016, 1905);
+
+    const d = new Date();
+    const yearRangeEnd = d.getFullYear();
+    const yearRangestar = yearRangeEnd - 100;
+    this.yearRange = `${yearRangestar}:${yearRangeEnd}`;
 
     if (!this.userService.getSyncProfile().profile_image) {
       this.userService.getSyncProfile().profile_image = Constants.img.avatar;
     }
-    this.handleSelectCropEvent();
 
+    let birthday: any = '';
     if (this.userService.getSyncProfile().birthday !== null) {
-      const birthday = new Date(this.userService.getSyncProfile().birthday);
-      this.birthdayDate.day = birthday.getDate();
-      this.birthdayDate.month = birthday.getMonth() + 1;
-      this.birthdayDate.year = birthday.getUTCFullYear();
+      birthday = new Date(this.userService.getSyncProfile().birthday);
     }
 
     this.form = fb.group({
@@ -97,14 +75,12 @@ export class MyProfileComponent implements OnInit, OnDestroy {
         Validators.compose([Validators.required, CustomValidator.blanked])
       ],
       'email': [
-        {value: this.userService.getSyncProfile().email, disabled: true},
+        { value: this.userService.getSyncProfile().email, disabled: true },
         Validators.compose([Validators.required, CustomValidator.emailFormat])
       ],
       'phone_prefix': [this.userService.getSyncProfile().nationality],
       'phone_number': [this.userService.getSyncProfile().phone_number],
-      'birthday_day': [this.birthdayDate.day],
-      'birthday_month': [this.birthdayDate.month],
-      'birthday_year': [this.birthdayDate.year]
+      'birthday': [birthday]
     });
 
     this.first_name = this.form.controls['first_name'];
@@ -112,9 +88,7 @@ export class MyProfileComponent implements OnInit, OnDestroy {
     this.email = this.form.controls['email'];
     this.phone_prefix = this.form.controls['phone_prefix'];
     this.phone_number = this.form.controls['phone_number'];
-    this.birthday_day = this.form.controls['birthday_day'];
-    this.birthday_month = this.form.controls['birthday_month'];
-    this.birthday_year = this.form.controls['birthday_year'];
+    this.birthday = this.form.controls['birthday'];
   }
 
   ngOnInit(): void {
@@ -143,6 +117,9 @@ export class MyProfileComponent implements OnInit, OnDestroy {
   }
 
   onSubmit(values: any): void {
+    const birthday_c = new Date(values.birthday);
+    // console.log(birthday_c.getDate(), birthday_c.getMonth() + 1, birthday_c.getUTCFullYear());
+
     // Set value after updating form (checking user leave this page)
     this.formValue = values;
 
@@ -159,9 +136,9 @@ export class MyProfileComponent implements OnInit, OnDestroy {
         last_name: values.last_name,
         nationality: values.phone_prefix,
         phone_number: values.phone_number,
-        birthday_day: values.birthday_day.toString(),
-        birthday_month: values.birthday_month.toString(),
-        birthday_year: values.birthday_year.toString(),
+        birthday_day: birthday_c.getDate().toString(),
+        birthday_month: (birthday_c.getMonth() + 1).toString(),
+        birthday_year: birthday_c.getUTCFullYear().toString(),
         sex: values.sex
       });
 
@@ -181,57 +158,6 @@ export class MyProfileComponent implements OnInit, OnDestroy {
     }
   }
 
-  uploadImage(event: any): void {
-    event.preventDefault();
-    // this.uploadProfile.modal.open();
-    this.commonEventService.broadcast({channel: 'SELECT_CROP_EVENT', action: 'SELECT_CROP:OPEN',
-     payload: {currentImage: this.userService.getSyncProfile().profile_image} });
-  }
-
-  handleSelectCropEvent() {
-    this.commonEventService.filter((event: any) => event.channel === 'SELECT_CROP_EVENT')
-      .pipe(takeUntil(this.destroySubject))
-      .subscribe((event: any) => {
-        this.doEvent(event);
-      });
-  }
-
-
-  doEvent(event: any) {
-    // console.log(event);
-    switch (event.action) {
-      case 'SELECT_CROP:DONE':
-        // Change user profile
-        this.updateProfileImageBase64(event.payload);
-        break;
-      default:
-        break;
-    }
-  }
-
-  updateProfileImageBase64(img: string): void {
-    this.updateUser(JSON.stringify({image: img}));
-  }
-
-  private updateUser(body: string): void {
-    this.userService.update(body)
-      .subscribe((result: any) => {
-          // stop loading
-          this.loadingService.stop();
-          this.toastsService.success(result.message);
-
-        //  reload profile image
-          $('img.lazyloaded').addClass('lazyload');
-        },
-        error => {
-          // stop loading
-          this.loadingService.stop();
-          this.toastsService.danger(this.errorMessage);
-          console.log(error);
-        }
-      );
-  }
-
   sendVerifyEmail() {
     this.apiBaseService.post(`users/confirmation`).subscribe((res: any) => {
       this.toastsService.success('A verification email was sent to your email address');
@@ -241,11 +167,5 @@ export class MyProfileComponent implements OnInit, OnDestroy {
   onCompleteChange(country: any) {
     this.form.controls['phone_prefix'].setValue(country.code);
     this.phoneChanged = true;
-  }
-
-  private range (start: number, end: number) {
-    const f: number = (end > start) ? start : end;
-    const res: number[] = Array.from(Array(Math.abs(end - start) + 1).keys()).map((i: number) => (i + f));
-    return (end > start) ? res : res.reverse();
   }
 }
