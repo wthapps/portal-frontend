e499c02af196e9f61300b820684f7b90ed5973df
An Vo <akira.an111@gmail.com>
2018-12-27 10:17:41 +0700

    update - all -User can receive chat notification after reconnecting network #ref WTHZONE-1095

---
+++ src/shared/services/page-visibility.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+
+/**
+ * This injectable class is a wrapper for the native Page Visibility API
+ * Ref: https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+ */
+@Injectable()
+export class PageVisibilityService {
+  hiddenState$: Observable<boolean>;
+
+  private hiddenStateSubject: Subject<boolean> = new Subject<boolean>();
+
+  constructor() {
+    this.hiddenState$ = this.hiddenStateSubject.asObservable();
+    this.registerVisibilityChange();
+  }
+
+  registerVisibilityChange() {
+    // Set the name of the hidden property and the change event for visibility
+    let hidden, visibilityChange;
+    if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
+      hidden = 'hidden';
+      visibilityChange = 'visibilitychange';
+    } else if (typeof document['msHidden'] !== 'undefined') {
+      hidden = 'msHidden';
+      visibilityChange = 'msvisibilitychange';
+    } else if (typeof document['webkitHidden'] !== 'undefined') {
+      hidden = 'webkitHidden';
+      visibilityChange = 'webkitvisibilitychange';
+    }
+
+    // Warn if the browser doesn't support addEventListener or the Page Visibility API
+    if (typeof document.addEventListener === 'undefined' || hidden === undefined) {
+      console.log('This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.');
+    } else {
+      // Handle page visibility change
+      document.addEventListener(visibilityChange, () => this.hiddenStateSubject.next(document[hidden]), false);
+
+    }
+  }
+
+}
