dc78c03f3ed2c2fdac2f3aa513a1aae6b0267fde
trungnghia112 <trungnghia112@gmail.com>
2018-12-27 12:40:22 +0700

    update - Account - Update layout - invitation #WTHZONE-1069

--- src/my-account/admin/invitations/invitations.component.ts
+++ src/my-account/admin/invitations/invitations.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoadingService } from '@shared/shared/components/loading/loading.service';
-import { ToastsService } from '@shared/shared/components/toast/toast-message.service';
 
 import { InvitationService } from '@wth/shared/shared/components/invitation/invitation.service';
+import { MessageService } from 'primeng/api';
+import { map } from 'rxjs/operators';
 
 declare let _: any;
 
@@ -22,7 +23,7 @@ export class MyInvitationsComponent implements OnInit, OnDestroy {
   currentTab: string; // pending, accepted
 
   constructor(private invitationService: InvitationService,
-              private toastsService: ToastsService,
+              private messageService: MessageService,
               private loadingService: LoadingService) {
     this.currentTab = 'pending';
     this.cols = [
@@ -32,17 +33,19 @@ export class MyInvitationsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.invitationService.getByStatus({ status: 'pending' })
-      .subscribe((res: any) => {
-        this.invPending = res.data;
-      });
-    this.invitationService.getByStatus({ status: 'accepted' })
-      .subscribe((res: any) => {
-        this.invAccepted = res.data;
-      });
+    this.getData().then();
   }
 
   ngOnDestroy() {
+  }
+
+  async getData() {
+    this.invPending = await this.invitationService.getByStatus({ status: 'pending' })
+      .pipe(map(res => res.data))
+      .toPromise();
+    this.invAccepted = await this.invitationService.getByStatus({ status: 'accepted' })
+      .pipe(map(res => res.data))
+      .toPromise();
   }
 
   doEvent(event: any) {
@@ -62,13 +65,22 @@ export class MyInvitationsComponent implements OnInit, OnDestroy {
     this.loadingService.start();
     const ids = _.map(selectedItems, 'id');
     this.invitationService.multiResend({ ids: ids }).toPromise()
-      .then(() => {
+      .then(
+        () => {
           this.loadingService.stop();
-          this.toastsService.success('You have just resent invitations successfully!');
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'You have just resent invitations successfully!'
+          });
         },
         () => {
           this.loadingService.stop();
-          this.toastsService.danger('There is a error when you resent invitations!');
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'There is a error when you resent invitations!'
+          });
         });
     this.clear();
   }
@@ -78,11 +90,12 @@ export class MyInvitationsComponent implements OnInit, OnDestroy {
     this.loadingService.start();
     const ids = _.map(selectedItems, 'id');
     this.invitationService.multiDelete({ 'ids': ids })
-      .subscribe(() => {
-        this.loadingService.stop();
-        _.remove(this.invAccepted, (i: any) => ids.indexOf(i.id) !== -1);
-        _.remove(this.invPending, (i: any) => ids.indexOf(i.id) !== -1);
-      });
+      .subscribe(
+        () => {
+          this.loadingService.stop();
+          _.remove(this.invAccepted, (i: any) => ids.indexOf(i.id) !== -1);
+          _.remove(this.invPending, (i: any) => ids.indexOf(i.id) !== -1);
+        });
     this.clear();
   }
 
