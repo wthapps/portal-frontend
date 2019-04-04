779ec966cc07603de9244a5cc479c0f7eaf7e048
trungnghia112 <trungnghia112@gmail.com>
2019-04-04 14:28:52 +0700

    update - Notes - [Mobile] Update layout of note detail on Shared with me #WTHZONE-1209

--- src/note/shared/modal/name/edit.component.ts
+++ src/note/shared/modal/name/edit.component.ts
@@ -1,26 +1,6 @@
-// import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-// import { BsModalComponent } from 'ng2-bs3-modal';
-//
-// @Component({
-//   selector: 'z-note-shared-modal-edit-name',
-//   templateUrl: 'edit.component.html'
-// })
-//
-// export class ZNoteSharedModalEditNameComponent {
-//   @ViewChild('modal') modal: BsModalComponent;
-//   @Input() name: string;
-//   @Output() eventEmitter: EventEmitter<any> = new EventEmitter<any>();
-//
-//   onSave() {
-//     this.eventEmitter.emit(this.name);
-//   }
-// }
-
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 
 import { BsModalComponent } from 'ng2-bs3-modal';
-
-declare var _: any;
 
 @Component({
   selector: 'z-note-shared-modal-edit-name',
@@ -29,6 +9,7 @@ declare var _: any;
 export class ZNoteSharedModalEditNameComponent implements OnInit, OnDestroy {
   @ViewChild('modal') modal: BsModalComponent;
   @Input() name: string;
+  @Input() permission: string;
   @Output() eventEmitter: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {
