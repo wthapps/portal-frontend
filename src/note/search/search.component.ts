a67efbe9cecebc847b8d5fa20a18f26ae98f15a3
trungnghia112 <trungnghia112@gmail.com>
2019-05-10 16:45:54 +0700

    update Notes - Update layout grid/list #WTHZONE-1219

--- src/note/search/search.component.ts
+++ src/note/search/search.component.ts
@@ -1,68 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-
-import { Observable } from 'rxjs';
-
-import { Folder } from '../shared/reducers/folder';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 
 import * as context from '../shared/reducers/context';
-import * as fromRoot from '../shared/reducers/index';
 import * as fromNote from '../shared/actions/note';
-import * as listReducer from '../shared/reducers/features/list-mixed-entities';
-import { UrlService } from '@shared/services/url.service';
 import { ApiBaseService } from '@shared/services/apibase.service';
-import { ZNoteService } from '@notes/shared/services/note.service';
-import { Note } from '@shared/shared/models/note.model';
-import { NoteConstants } from '@notes/shared/config/constants';
+import { NoteConstants, noteConstants } from '@notes/shared/config/constants';
 
 @Component({
   selector: 'z-note-search',
   templateUrl: 'search.component.html'
 })
-export class ZNoteSearchComponent implements OnInit, OnDestroy {
-  readonly noteConstants: NoteConstants = new NoteConstants();
+export class ZNoteSearchComponent implements OnInit {
 
-  noteItems$: Observable<Note[]>;
-  folderItems$: Observable<Folder[]>;
-  sortOption$: Observable<any>;
-  nodeState$: Observable<any>;
-  selectedObjects$: Observable<any[]>;
-  selectAll$: Observable<boolean>;
-  currentFolderPath$: Observable<any[]>;
-  // viewMode$: Observable<any>;
-  loading$: Observable<boolean>;
-  context$: Observable<any>;
+  noteConstants: NoteConstants = noteConstants;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private urlService: UrlService,
-    private noteService: ZNoteService,
-    private store: Store<any>,
-    private apiBaseService: ApiBaseService
-  ) {
-    this.noteItems$ = this.store.select(listReducer.getNotes);
-    this.folderItems$ = this.store.select(listReducer.getFolders);
-    this.sortOption$ = this.store.select(fromRoot.getSortOption);
-    // this.viewMode$ = this.store.select(fromRoot.getViewMode);
-    this.context$ = this.store.select(context.getContext);
-    this.selectedObjects$ = this.store.select(fromRoot.getSelectedObjects);
-    this.currentFolderPath$ = this.store.select(fromRoot.getCurrentFolderPath);
-    this.selectAll$ = this.store.select(fromRoot.getSelectAll);
-    this.loading$ = this.store.select(fromRoot.getLoading);
-
+  constructor(private store: Store<any>,
+              private route: ActivatedRoute,
+              private apiBaseService: ApiBaseService) {
     this.route.queryParamMap.forEach(queryParamMap => {
       this.getSearchResult(queryParamMap.get('q'));
-    })
+    });
   }
 
   ngOnInit() {
-    //
-  }
-
-  ngOnDestroy() {
   }
 
   getSearchResult(q: string): Promise<any> {
@@ -74,21 +36,11 @@ export class ZNoteSearchComponent implements OnInit, OnDestroy {
           type: context.SET_CONTEXT,
           payload: {
             page: this.noteConstants.PAGE_SEARCH,
-            pathTitle: 'Search',
+            pathTitle: 'Search result',
             permissions: this.noteConstants.PAGE_PERMISSIONS.SEARCH,
             noData: this.noteConstants.NO_DATA.SEARCH
           }
         });
       });
-  }
-
-  onNewNote() {
-    this.noteService.modalEvent({ action: 'note:open_note_add_modal' });
-  }
-
-  onFolder() {
-    this.noteService.modalEvent({
-      action: 'note:folder:create'
-    });
   }
 }
--- src/note/trash/trash.component.ts
+++ src/note/trash/trash.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, HostBinding, ViewChild } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-
-import { ZNoteService } from '../shared/services/note.service';
 import * as fromRoot from '../shared/reducers/index';
 import * as context from '../shared/reducers/context';
 import * as note from '../shared/actions/note';
 import { Note } from '@shared/shared/models/note.model';
 import * as listReducer from '../shared/reducers/features/list-mixed-entities';
 import { noteConstants } from '@notes/shared/config/constants';
+import { WDataViewComponent } from '@shared/components/w-dataView/w-dataView.component';
+import { Constants } from '@shared/constant';
+import { WthConfirmService } from '@shared/shared/components/confirmation/wth-confirm.service';
+import { CommonEventService } from '@shared/services';
+
+declare var _: any;
+const OBJECT_TYPE = noteConstants.OBJECT_TYPE;
 
 @Component({
   selector: 'z-note-trash',
   templateUrl: 'trash.component.html'
 })
 export class ZNoteTrashComponent implements OnInit {
+  @HostBinding('class') class = 'main-page-body';
+  @ViewChild('dataView') dataView: WDataViewComponent;
+
   noteItems$: Observable<Note[]>;
   folderItems$: Observable<any>;
-  sortOption$: Observable<any>;
-  nodeState$: Observable<any>;
   selectedObjects$: Observable<any[]>;
   isSelectAll$: Observable<boolean>;
   allItems$: Observable<any[]>;
@@ -30,10 +35,37 @@ export class ZNoteTrashComponent implements OnInit {
 
   readonly PAGE_TYPE: any = noteConstants.PAGE_TRASH;
 
-  constructor(
-    private store: Store<fromRoot.State>,
-    private noteService: ZNoteService
-  ) {}
+  readonly tooltip: any = Constants.tooltip;
+  data$: Observable<any[]>;
+  menuActions = {
+    restore: {
+      active: true,
+      icon: 'fa fa-history',
+      text: this.tooltip.restore,
+      action: 'restore'
+    },
+    permanentDelete: {
+      active: true,
+      icon: 'fa fa-trash-o',
+      text: this.tooltip.permanentDelete,
+      action: 'permanentDelete'
+    }
+  };
+
+  otherActions = {
+    emptyTrash: {
+      active: true,
+      icon: 'fa fa-trash',
+      text: 'Empty Trash',
+      action: 'emptyTrash'
+    }
+  };
+
+  constructor(private store: Store<fromRoot.State>,
+              private wthConfirm: WthConfirmService,
+              public commonEventService: CommonEventService) {
+    this.data$ = this.store.select(listReducer.getAllItems);
+  }
 
   ngOnInit() {
     this.noteItems$ = this.store.select(listReducer.getNotes);
@@ -54,17 +86,124 @@ export class ZNoteTrashComponent implements OnInit {
     this.store.dispatch({ type: note.TRASH_LOAD });
   }
 
-  onNewNote() {
-    this.noteService.modalEvent({
-      action: 'note:open_note_add_modal',
-      payload: { parent_id: null }
-    });
+  onSelectCompleted() {
+
+    // update icon favorite
+    // this.updateMenuFavorite(_.every(this.dataView.selectedDocuments, 'favorite'));
+
+    // check menu view
+    // this.validatePermission();
   }
 
-  onFolder() {
-    this.noteService.modalEvent({
-      action: 'note:folder:create',
-      payload: { parent_id: null }
+  onDblClick(event: any) {
+    if (event.object_type === OBJECT_TYPE.FOLDER) {
+      this.dataView.container.clearSelection();
+
+      this.wthConfirm.confirm({
+        acceptLabel: 'Restore',
+        rejectLabel: 'Cancel',
+        message: `To view this folder, you'll need to restore it from your trash`,
+        header: 'This folder is in your trash',
+        accept: () => {
+          this.store.dispatch({ type: note.RESTORE, payload: [event] });
+        }
+      });
+    }
+  }
+
+  onMenuAction(action: string) {
+    const selectedObjects = this.dataView.selectedDocuments;
+    const objects = this.dataView.selectedDocuments.map(({ id, object_type, favourite }) => ({
+      id,
+      object_type,
+      favourite
+    }));
+    switch (action) {
+      case 'restore': {
+        this.store.dispatch({ type: note.RESTORE, payload: selectedObjects });
+        this.commonEventService.broadcast({
+          action: 'update',
+          channel: 'noteLeftMenu',
+          payload: selectedObjects
+        });
+        break;
+      }
+      case 'permanentDelete': {
+        if (selectedObjects.length >= 1) {
+          let message = `
+                        You are about to delete Folder(s).<br>
+                        Once deleted - you cannot Undo deleting.<br>
+                        Folder and all included Notes and sub-folders will be permanently deleted
+          `;
+          let header = 'Delete Note and Folder';
+
+          if (this.isOnlyNote(selectedObjects)) {
+            message = `
+                        You are about to delete Note(s).<br> 
+                        Once deleted - you cannot Undo deleting.<br>
+                        Notes will be permanently deleted
+            `;
+            header = 'Delete Note';
+          }
+
+          if (this.isOnlyFolder(selectedObjects)) {
+            header = 'Delete Folder';
+          }
+
+          this.wthConfirm.confirm({
+            message: message,
+            header: header,
+            accept: () => {
+              this.store.dispatch({
+                type: note.PERMANENT_DELETE,
+                payload: selectedObjects
+              });
+            }
+          });
+        }
+        break;
+      }
+      case 'emptyTrash': {
+        this.wthConfirm.confirm({
+          acceptLabel: 'Delete',
+          rejectLabel: 'Cancel',
+          message: `
+              All notes and folders in your trash will be permanently deleted.
+              <br>
+              <strong>This action cannot be undone</strong>
+          `,
+          header: 'Empty Trash',
+          accept: () => {
+            this.store.dispatch({ type: note.EMPTY_ALL });
+          }
+        });
+        break;
+      }
+      default: {
+        console.warn('unhandled action: ', action);
+      }
+    }
+  }
+
+  private isOnlyNote(selectedObjects): boolean {
+    let result = true;
+    _.forEach(selectedObjects, (item: any) => {
+      if (item.object_type === OBJECT_TYPE.FOLDER) {
+        result = false;
+        return;
+      }
     });
+    return result;
+  }
+
+  private isOnlyFolder(selectedObjects): boolean {
+    let result = true;
+    _.forEach(selectedObjects, (item: any) => {
+      if (item.object_type === OBJECT_TYPE.NOTE) {
+        result = false;
+        return;
+      }
+    });
+    return result;
   }
 }
