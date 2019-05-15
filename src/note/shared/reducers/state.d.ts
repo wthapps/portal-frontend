a0bf2c86855eb9561223e2c2e876b9a81ed5ae4e
An Vo <akira.an111@gmail.com>
2019-05-15 10:30:18 +0700

    update- note - remove unused mix-entities reducers/effects from store #ref WTHZONE-1220

--- src/note/shared/reducers/state.d.ts
+++ src/note/shared/reducers/state.d.ts
@@ -1,11 +1,9 @@
 import * as fromNote from './note';
 import * as fromFolder from './folder';
 import * as context from './context';
-import * as fromMixedEntity from '../mixed-enity/mixed-entity.reducer';
 
 export interface AppState {
   notes: fromNote.State;
   folders: fromFolder.State;
-  mixedEntity: fromMixedEntity.State;
   context: any;
 }
